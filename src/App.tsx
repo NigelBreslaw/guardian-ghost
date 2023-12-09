af1bb551f2192771db072de583df20f690ff74e4
Nigel Breslaw <nigel.breslaw@gmail.com>
2023-12-09 23:26:02 +0200

    Crude start to auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react"
+import { deleteCookie, getCookie, setCookie } from "./Utilities"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+
+  const cookieName = "auth-state"
+  const originalState = useRef<string>(getCookie(cookieName) || crypto.randomUUID())
+
+  const startAuth = () => {
+    const bungieURL = `https://www.bungie.net/en/oauth/authorize?client_id=45432&response_type=code&state=${originalState.current}`
+    console.log(bungieURL)
+    window.location.href = bungieURL
+  }
+
+  useEffect(() => {
+    // Function to parse URL parameters
+    const getCodeFromUrl = (url: URL): string | null => {
+      const state = url.searchParams.get("state")
+
+      if (originalState.current === state) {
+        const code = url.searchParams.get("code")
+        if (code) {
+          return code
+        } else {
+          throw new Error("No code in URL")
+        }
+      } else {
+        throw new Error("State mismatch")
+      }
+    }
+
+    const currentUrl = new URL(window.location.href);
+    console.log("current:", currentUrl)
+    const urlParams = getCodeFromUrl(currentUrl);
+
+    console.log("orig:", originalState.current)
+
+    // Check if 'state' matches the original state
+    if (urlParams) {
+      // Perform actions with 'code' and 'state'
+      console.log("Authorization successful!")
+      console.log("Code:", urlParams)
+      // console.log('State:', state);
+      // Clear the state cookie once used
+      deleteCookie(cookieName)
+    } else {
+      console.error("Authorization failed. State mismatch.")
+    }
+  }, [])
+
+  useEffect(() => {
+    // Save the original state to a cookie with SameSite attribute. And expires using a date time in 15 minutes
+    setCookie(cookieName, originalState.current,  { sameSite: "strict", expires: new Date(Date.now() + 15 * 60 * 1000)})
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <div></div>
+      <h1>Ishtar For Web</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={startAuth}>Auth</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Utilities.ts
@@ -0,0 +1,49 @@
+type CookieOptions = {
+  expires?: Date
+  path?: string
+  domain?: string
+  secure?: boolean
+  sameSite?: "strict" | "lax" | "none"
+}
+
+export const setCookie = (name: string, value: string, options?: CookieOptions) => {
+  let cookieString = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`
+
+  if (options) {
+    if (options.expires instanceof Date) {
+      cookieString += `; expires=${options.expires.toUTCString()}`
+    }
+    if (options.path) {
+      cookieString += `; path=${options.path}`
+    }
+    if (options.domain) {
+      cookieString += `; domain=${options.domain}`
+    }
+    if (options.secure) {
+      cookieString += "; secure"
+    }
+    if (options.sameSite) {
+      cookieString += `; samesite=${options.sameSite}`
+    }
+  }
+
+  document.cookie = cookieString
+}
+
+export const getCookie = (cookieName: string): string | null => {
+  const cookies = document.cookie.split(";")
+
+  for (const cookie of cookies) {
+    const [name, value] = cookie.trim().split("=")
+
+    if (name === `${encodeURIComponent(cookieName)}`) {
+      return decodeURIComponent(value)
+    }
+  }
+
+  return null
+}
+
+export const deleteCookie = (name: string) => {
+  document.cookie = `${encodeURIComponent(name)}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+}
