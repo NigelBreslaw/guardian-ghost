a3d01b07787e10b8d0cce3a29765a8c18376f241
Nigel Breslaw <nigel.breslaw@gmail.com>
2023-12-09 18:59:31 +0200

    Plugin to allow https on localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
+  server: {
+    port: 7777,
+    open: true,
+    host: '127.0.0.1'
+  }
 })
