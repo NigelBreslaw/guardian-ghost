e574a350f5742c77456667b1bc6dd05dc8681236
Nigel Breslaw <nigel.breslaw@gmail.com>
2023-12-26 22:39:06 +0100

    Action to deploy dashboard

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
       </div>
-      <h1>Placeholder for dashboard</h1>
+      <h1>Placeholder for dashboard!</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
