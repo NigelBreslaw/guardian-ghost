08a7aebbb1ddf7ffc9fbdf77ed432906b8747f99
Nigel Breslaw <nigel.breslaw@gmail.com>
2023-12-26 13:28:49 +0100

    Tweak docs

--- docs/starlight/astro.config.mjs
+++ docs/starlight/astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'GG Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/NigelBreslaw/guardian-ghost',
 			},
 			sidebar: [
 				{
