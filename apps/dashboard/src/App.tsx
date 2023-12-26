a5ec7ba137933a22ee0d0bb78fc39af88a601ce8
Nigel Breslaw <nigel.breslaw@gmail.com>
2023-12-26 23:16:41 +0100

    Apply linting everywhere

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -1,33 +1,30 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal } from "solid-js";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import solidLogo from "./assets/solid.svg";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+	const [count, setCount] = createSignal(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Placeholder for dashboard!</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Developer stats coming soon.
-        </p>
-      </div>
-
-    </>
-  )
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} class="logo" alt="Vite logo" />
+				</a>
+				<a href="https://solidjs.com" target="_blank" rel="noreferrer">
+					<img src={solidLogo} class="logo solid" alt="Solid logo" />
+				</a>
+			</div>
+			<h1>Placeholder for dashboard!</h1>
+			<div class="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count()}
+				</button>
+				<p>Developer stats coming soon.</p>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
