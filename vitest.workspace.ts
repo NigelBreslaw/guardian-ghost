fe9d882e8181a3971e1d0016628baf9cc9b942c5
Nigel Breslaw <nigel.breslaw@gmail.com>
2023-12-27 13:25:15 +0100

    simplify

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,12 +1,4 @@
-import { defineWorkspace } from 'vitest/config'
-
-// defineWorkspace provides a nice type hinting DX
-export default defineWorkspace([
-  'apps/*',
-  "docs/*",
-  {
-    test: {
-      
-    }
-  }
-])
+export default [
+    "apps/*",
+    "docs/*"
+]
