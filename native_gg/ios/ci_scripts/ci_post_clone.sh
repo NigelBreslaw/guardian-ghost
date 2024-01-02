8a0ec7388ea6e8266f4d3421d494c2345f5dada8
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-02 17:18:43 +0200

    Fix Xcode cloud and CI
    * Fix script
    * Stop github ci running twice

--- native_gg/ios/ci_scripts/ci_post_clone.sh
+++ native_gg/ios/ci_scripts/ci_post_clone.sh
@@ -6,8 +6,11 @@
 #  Created by NigelBreslaw on 2.1.2024.
 #
 
+echo "============> Installing NVM <============"
+brew install nvm
+
 echo "============> Installing Node <============"
-brew install node
+nvm install 20.10.0
 
 echo "=========> Installing cocoapods"
 brew install cocoapods
@@ -18,5 +21,8 @@ npm install -g pnpm
 echo "=====> Moving to build directory"
 cd native_gg
 
-echo "===> Install pods"
+echo "=> Install npm dependencies"
+pnpm i --frozen-lockfile
+
+echo "=> Install pods"
 pnpm run pod-install
