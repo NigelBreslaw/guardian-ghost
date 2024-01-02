d459445ee969d467e7ac34b63437037dd7679f2e
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-02 19:22:56 +0200

    Try script again

--- native_gg/ios/ci_scripts/ci_post_clone.sh
+++ native_gg/ios/ci_scripts/ci_post_clone.sh
@@ -6,52 +6,39 @@
 #  Created by NigelBreslaw on 2.1.2024.
 #
 
-brew install frum
-frum install 3.3.0
-ruby -v
 
-# arch
+arch
+node_version=20.10.0
 
-# if [ "$ARCHITECTURE" == "arm64" ]; then
-#     # For arm64 architecture (Apple Silicon)
-#     arch=arm64
-# else
-#     # For x86 architecture
-#     arch=x64
-# fi
+if [ "$ARCHITECTURE" == "arm64" ]; then
+    # For arm64 architecture (Apple Silicon)
+    arch=arm64
+else
+    # For x86 architecture
+    arch=x64
+fi
 
-# echo "============> Installing Node <============"
-# curl "https://nodejs.org/dist/v20.10.0/node-v20.10.0-darwin-$arch.tar.gz" -o $HOME/Downloads/node.tar.gz
-# tar -xf "$HOME/Downloads/node.tar.gz"
-# NODE_PATH="$PWD/node-v20.10.0-darwin-$arch/bin"
-# PATH+=":$NODE_PATH"
-# export PATH
-# node -v
-# npm -v
+echo "============> Installing Node <============"
+curl "https://nodejs.org/dist/v$node_version/node-v$node_version-darwin-$arch.tar.gz" -o $HOME/Downloads/node.tar.gz
+tar -xf "$HOME/Downloads/node.tar.gz"
+NODE_PATH="$PWD/node-v$node_version-darwin-$arch/bin"
+PATH+=":$NODE_PATH"
+export PATH
+node -v
+npm -v
 
-# echo "============> Installing chruby <============"
-# brew install chruby ruby-install
+echo "============> Installing cocoapods <============"
+brew install cocoapods
 
-# echo "============> Installing ruby <============"
-# ruby-install ruby 3.3
+echo "=======> Installing PNPM"
+npm install -g pnpm@8.13.1
 
 
+echo "=====> Moving to build directory"
+cd native_gg
 
-# echo "============> Installing cocoapods <============"
-# brew install cocoapods
+echo "=> Install npm dependencies"
+pnpm i --frozen-lockfile
 
-
-
-
-# echo "=======> Installing PNPM"
-# npm install -g pnpm@8.13.1
-
-
-# echo "=====> Moving to build directory"
-# cd native_gg
-
-# echo "=> Install npm dependencies"
-# pnpm i --frozen-lockfile
-
-# echo "=> Install pods"
-# pnpm run pod-install
+echo "=> Install pods"
+pnpm run pod-install
