3ac4c998d3eedf29bc91611f24ebe9893243eb02
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-02 20:15:31 +0200

    Working post clone script for Xcode cloud

--- native_gg/ios/ci_scripts/ci_post_clone.sh
+++ native_gg/ios/ci_scripts/ci_post_clone.sh
@@ -6,17 +6,33 @@
 #  Created by NigelBreslaw on 2.1.2024.
 #
 
-echo "============> Installing NVM <============"
-brew install nvm
+
+arch
+node_version=20.10.0
+
+if [ "$ARCHITECTURE" == "arm64" ]; then
+    # For arm64 architecture (Apple Silicon)
+    arch=arm64
+else
+    # For x86 architecture
+    arch=x64
+fi
 
 echo "============> Installing Node <============"
-nvm install 20.10.0
+curl "https://nodejs.org/dist/v$node_version/node-v$node_version-darwin-$arch.tar.gz" -o $HOME/Downloads/node.tar.gz
+tar -xf "$HOME/Downloads/node.tar.gz"
+NODE_PATH="$PWD/node-v$node_version-darwin-$arch/bin"
+PATH+=":$NODE_PATH"
+export PATH
+node -v
+npm -v
 
-echo "=========> Installing cocoapods"
+echo "============> Installing cocoapods <============"
 brew install cocoapods
 
 echo "=======> Installing PNPM"
-npm install -g pnpm
+npm install -g pnpm@8.13.1
+
 
 echo "=====> Moving to build directory"
 cd native_gg
