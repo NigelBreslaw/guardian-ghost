2d82734d84228034b7f85b0f064cba771cc2fba1
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-02 18:14:49 +0200

    Try a faster install of node and cocoapods

--- native_gg/ios/ci_scripts/ci_post_clone.sh
+++ native_gg/ios/ci_scripts/ci_post_clone.sh
@@ -6,17 +6,32 @@
 #  Created by NigelBreslaw on 2.1.2024.
 #
 
+pushd ..
+ 
+echo ">>> SETUP ENVIRONMENT"
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+ 
+echo ">>> INSTALL BUNDLER"
+gem install bundler --install-dir $GEM_HOME
+
 echo "============> Installing Node <============"
-brew install node@20
+curl "https://nodejs.org/dist/v20.10.0/node-v20.10.0-darwin-arm64.tar.gz" -o $HOME/Downloads/node.tar.gz
+tar -xf "$HOME/Downloads/node.tar.gz"
+NODE_PATH="$PWD/node-$VERSION-darwin-$ARCH/bin"
+PATH+=":$NODE_PATH"
+export PATH
+node -v
+npm -v
 
-echo "============> Linking Node <============"
-brew link node@20
+popd
 
-echo "=========> Installing cocoapods"
-brew install cocoapods && bundle update --bundler
 
 echo "=======> Installing PNPM"
 npm install -g pnpm@8.13.1
+
 
 echo "=====> Moving to build directory"
 cd native_gg
