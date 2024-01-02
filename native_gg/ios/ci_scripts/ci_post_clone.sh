a4a2a91b7cbaf1776e81469f7bc7a317842e953c
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-02 18:52:44 +0200

    Try right arch

--- native_gg/ios/ci_scripts/ci_post_clone.sh
+++ native_gg/ios/ci_scripts/ci_post_clone.sh
@@ -6,10 +6,20 @@
 #  Created by NigelBreslaw on 2.1.2024.
 #
 
+arch
+
+if [ "$ARCHITECTURE" == "arm64" ]; then
+    # For arm64 architecture (Apple Silicon)
+    arch=arm64
+else
+    # For x86 architecture
+    arch=x64
+fi
+
 echo "============> Installing Node <============"
-curl "https://nodejs.org/dist/v20.10.0/node-v20.10.0-darwin-arm64.tar.gz" -o $HOME/Downloads/node.tar.gz
+curl "https://nodejs.org/dist/v20.10.0/node-v20.10.0-darwin-$arch.tar.gz" -o $HOME/Downloads/node.tar.gz
 tar -xf "$HOME/Downloads/node.tar.gz"
-NODE_PATH="$PWD/node-v20.10.0-darwin-arm64/bin"
+NODE_PATH="$PWD/node-v20.10.0-darwin-$arch/bin"
 PATH+=":$NODE_PATH"
 export PATH
 node -v
