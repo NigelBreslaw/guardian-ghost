35f4a2860f6c499fb8bb6caec11944f5ae330fb3
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-03 22:08:30 +0200

    Fix linting

--- gg-mono/examples/default-project/src/App.tsx
+++ gg-mono/examples/default-project/src/App.tsx
@@ -1,7 +1,7 @@
 import { createSignal } from "solid-js";
-import solidLogo from "./assets/solid.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import solidLogo from "./assets/solid.svg";
 
 function App() {
     const [count, setCount] = createSignal(0);
@@ -9,21 +9,20 @@ function App() {
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} class="logo" alt="Vite logo" />
                 </a>
-                <a href="https://solidjs.com" target="_blank">
+                <a href="https://solidjs.com" target="_blank" rel="noreferrer">
                     <img src={solidLogo} class="logo solid" alt="Solid logo" />
                 </a>
             </div>
-            <h1>Vite + Solid</h1>
+            <h1>Placeholder for dashboard!</h1>
             <div class="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count()}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
+                <button type="button" onClick={() => setCount((count) => count + 1)}>
+                    count is {count()}
+                </button>
+                <p>Developer stats coming soon.</p>
             </div>
-            <p class="read-the-docs">Click on the Vite and Solid logos to learn more</p>
         </>
     );
 }
