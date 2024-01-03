687a9e4ac9052945ea517588a14abb1e48f412d9
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-03 22:19:29 +0200

    Tweak projects

--- gg-mono/examples/default-project/vite.config.ts
+++ gg-mono/examples/default-project/vite.config.ts
@@ -3,4 +3,11 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  server: {
+    open: true,
+    host: '127.0.0.1'
+  },
+  build: {
+    sourcemap: true,
+  }
 })
