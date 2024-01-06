2f514125d0e35b5a928b7a37fff840f029c245b6
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-06 22:46:53 +0200

    Tidy more files

--- gg-mono/docs/starlight/astro.config.mjs
+++ gg-mono/docs/starlight/astro.config.mjs
@@ -3,10 +3,12 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.guardianghost.com',
 	integrations: [
 		starlight({
 			title: 'GG Docs',
 			social: {
+				email: 'mailto:support@guardianghost.com',
 				twitter: 'https://twitter.com/GGuardianGhost',
 				github: 'https://github.com/NigelBreslaw/guardian-ghost',
 			},
@@ -14,9 +16,9 @@ export default defineConfig({
 				{
 					label: 'Guides',
 					items: [
-						{ label: 'Getting started', link: '/guides/getting-started/' },
+						{ label: 'Monorepo', link: '/guides/monorepo/' },
 						{ label: 'Cloudflare', link: '/guides/cloudflare/' },
-						{ label: 'CI', link: '/guides/ci-maintenance/' },
+						{ label: 'CI Maintenance', link: '/guides/ci-maintenance/' },
 						{ label: 'Dependabot', link: '/guides/dependabot/' },
 						{ label: 'CI', link: '/guides/ci/' },
 						{ label: 'Misc', link: '/guides/misc/' },
