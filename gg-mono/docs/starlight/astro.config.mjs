7a307d11e417eeafc94466f757d7a3b6e51ffcdf
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-06 22:55:04 +0200

    Simplify docs (#107)
    The docs were over complex for such an early stage.

--- gg-mono/docs/starlight/astro.config.mjs
+++ gg-mono/docs/starlight/astro.config.mjs
@@ -3,41 +3,28 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.guardianghost.com',
 	integrations: [
 		starlight({
 			title: 'GG Docs',
 			social: {
+				email: 'mailto:support@guardianghost.com',
+				twitter: 'https://twitter.com/GGuardianGhost',
 				github: 'https://github.com/NigelBreslaw/guardian-ghost',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						{ label: 'Getting started', link: '/guides/getting-started/' },
+						{ label: 'Monorepo', link: '/guides/monorepo/' },
 						{ label: 'Cloudflare', link: '/guides/cloudflare/' },
-
+						{ label: 'CI Maintenance', link: '/guides/ci-maintenance/' },
+						{ label: 'Dependabot', link: '/guides/dependabot/' },
+						{ label: 'CI', link: '/guides/ci/' },
+						{ label: 'Misc', link: '/guides/misc/' },
+						{ label: 'React Native', link: '/guides/react-native/' },
+						{ label: 'Costs', link: '/guides/costs/' },
 					],
-				},
-				{
-					label: 'Technologies',
-					items: [
-						{ label: 'CI', link: '/tech/ci/' },
-						{ label: 'Misc', link: '/tech/misc/' },
-					]
-				},
-				{
-					label: 'Setup',
-					items: [
-						{ label: 'CI', link: '/setup/ci/' },
-						{ label: 'Dependabot', link: '/setup/dependabot/' },
-					]
-				},
-				{
-					label: 'Reference',
-					items: [
-						{ label: 'React Native', link: '/reference/react-native/' },
-						{ label: 'Costs', link: '/reference/costs/' },
-					]
 				},
 			],
 		}),
