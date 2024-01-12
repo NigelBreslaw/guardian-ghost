a9a404755b2a30102cf8eb3806d8f299feb0305a
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-01-12 14:14:47 +0200

    Tidy?

--- gg-mono/apps/dashboard/astro.config.mjs
+++ gg-mono/apps/dashboard/astro.config.mjs
@@ -8,16 +8,6 @@ const DEV_PORT = 2121;
 // https://astro.build/config
 export default defineConfig({
 	site: "https://dashboard.guardianghost.com",
-	assetsPrefix: "_astro/",
-	// site: process.env.CI
-	// 	? 'https://themesberg.github.io'
-	// 	: `http://localhost:${DEV_PORT}`,
-	// base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-
-	// output: 'static',
-
-	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	// trailingSlash: 'always',
 
 	server: {
 		/* Dev. server only */
