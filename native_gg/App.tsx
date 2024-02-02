56d5847955f9560f9d85e86570f752e4979e4a43
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-02 14:55:41 +0100

    Working typescript

---
+++ native_gg/App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#232526", "#66686a"]}
+        style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0 }}
+      />
+      <Text style={{ color: "#fff" }}>Open up App.tsx to start working on your app!</Text>
+      <Image
+        style={{ width: 200, height: 200 }}
+        contentFit="contain"
+        source="https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg"
+      />
+
+      <Text style={{ fontSize: 22, marginTop: 15, color: "#150f63" }}>
+        New Architecture: <Text style={{ fontWeight: "bold" }}>Enabled</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
