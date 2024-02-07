c2f687073d5d3a462c7d88b4c351a4583eccc590
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-07 14:18:51 +0200

    Not needed

--- native_gg/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { ExpoConfig, ConfigContext } from "expo/config";
-
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  slug: "native_gg",
-  name: "native_gg",
-});
