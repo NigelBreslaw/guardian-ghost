177f43821f959154b03730dd6809a3ddf92fd278
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-07 23:39:59 +0200

    Tidy code

--- native_gg/App.tsx
+++ native_gg/App.tsx
@@ -1,68 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
-import * as Linking from "expo-linking";
-import * as WebBrowser from "expo-web-browser";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Auth from "./src/Auth";
 
 export default function App() {
-  const url = Linking.useURL();
-  const env = process.env.NODE_ENV;
-  const localAppID = "46261";
-  const realAppID = "46250";
-  const localRedirectURL = "https://localhost:19006/auth";
-  const realRedirectURL = "https://guardianghost.com/auth";
-
-  const redirectURL = env === "development" ? localRedirectURL : realRedirectURL;
-
-  const appID = env === "development" ? localAppID : realAppID;
-
   const [token, setToken] = useState("");
-
-  function processURL(url: string) {
-    console.log(Platform.OS, url);
-    if (Platform.OS === "web") {
-      const { hostname, path, queryParams } = Linking.parse(url);
-      console.log("hostname", hostname, "path", path, "queryParams", queryParams);
-      if (queryParams?.code) {
-        console.log("yay:", queryParams?.code);
-        setToken(queryParams.code.toString());
-      } else {
-        console.log("boooo");
-      }
-    }
-  }
-
-  useEffect(() => {
-    if (url) {
-      const { hostname, path, queryParams } = Linking.parse(url);
-      console.log("useEffect", Platform.OS, process.env.NODE_ENV);
-      if (queryParams?.code) {
-        console.log("ue: code:", queryParams?.code);
-        setToken(queryParams.code.toString());
-
-        if (Platform.OS === "ios") {
-          WebBrowser.dismissAuthSession();
-        }
-
-        if (Platform.OS === "web") {
-          console.log("maybeCompleteAuthSession");
-          WebBrowser.maybeCompleteAuthSession();
-        }
-      }
-    }
-  }, [url]);
-
-  function openURL(url: string) {
-    console.log("Opening URL in WebBrowser!");
-    WebBrowser.openAuthSessionAsync(url, `${redirectURL}`).then((result) => {
-      console.log("hi nigel", result);
-      if (result?.type === "success") {
-        processURL(result.url);
-      }
-    });
-  }
 
   return (
     <View style={styles.container}>
@@ -76,12 +20,7 @@ export default function App() {
         contentFit="contain"
         source="https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg"
       />
-      <Button
-        title="Auth"
-        onPress={() =>
-          openURL(`https://www.bungie.net/en/oauth/authorize?client_id=${appID}&response_type=code&reauth=true`)
-        }
-      />
+      <Auth token={token} setToken={setToken} />
       <Text style={{ fontSize: 22, marginTop: 15, color: "#150f63" }}>
         Auth token:: <Text style={{ fontWeight: "bold" }}>{token}</Text>
       </Text>
