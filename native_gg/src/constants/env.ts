d84d20eabd4a25ed1b2a08877c4088527ac7e0d7
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-10 14:58:11 +0200

    Remove secrets

--- native_gg/src/constants/env.ts
+++ native_gg/src/constants/env.ts
@@ -3,9 +3,9 @@ import { Platform } from "react-native";
 // @ts-ignore
 export const apiKey = process.env.EXPO_PUBLIC_API_KEY;
 // @ts-ignore
-export const clientID = "46261"; //process.env.EXPO_PUBLIC_CLIENT_ID;
+export const clientID = process.env.EXPO_PUBLIC_CLIENT_ID;
 // @ts-ignore
-export const clientSecret = "qVT6lYzc-t00NiqSRJc8lwbqdXuDMAweEt5-QDF3SU0"; //process.env.EXPO_PUBLIC_CLIENT_SECRET;
+export const clientSecret = process.env.EXPO_PUBLIC_CLIENT_SECRET;
 
 const isLocalWeb = process.env.NODE_ENV === "development" && Platform.OS === "web";
 
