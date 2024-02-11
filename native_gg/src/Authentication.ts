aba076fd08c0a90bbd1337b19f525ce6f0f41585
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-11 16:09:36 +0100

    Get the Oauth Refresh token (#345)
    * Warn on missing .env
    * Add typechecking to CI
    * Add base-64 package
    * Simplify

--- native_gg/src/Authentication.ts
+++ native_gg/src/Authentication.ts
@@ -1,20 +1,22 @@
-import { clientID, clientSecret } from "./constants/env.ts";
+import { apiKey, clientID, clientSecret } from "./constants/env.ts";
 import * as v from "valibot";
+import * as base64 from "base-64";
 
-const authJWTSchema = v.object({
+const refreshTokenSchema = v.object({
   access_token: v.string(),
   expires_in: v.number(),
   membership_id: v.string(),
   refresh_expires_in: v.number(),
   refresh_token: v.string(),
+  time_stamp: v.optional(v.string([v.isoTimestamp()])),
   token_type: v.string(),
 });
 
-type InitialAuthJWT = v.Output<typeof authJWTSchema>;
+type RefreshToken = v.Output<typeof refreshTokenSchema>;
 
 export function handleAuthCode(code: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    getAuthToken(code)
+    getRefreshToken(code)
       .then((initialJWT) => {
         const membership_id = processInitialAuthJWT(initialJWT);
 
@@ -26,7 +28,7 @@ export function handleAuthCode(code: string): Promise<string> {
   });
 }
 
-export function getAuthToken(bungieCode: string): Promise<JSON> {
+export function getRefreshToken(bungieCode: string): Promise<JSON> {
   const headers = new Headers();
   headers.append("Content-Type", "application/x-www-form-urlencoded");
 
@@ -56,10 +58,62 @@ export function getAuthToken(bungieCode: string): Promise<JSON> {
   });
 }
 
-function processInitialAuthJWT(jwtToken: unknown): string {
+export function getAccessToken(token: RefreshToken): Promise<RefreshToken> {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/x-www-form-urlencoded");
+  headers.append("X-API-Key", apiKey);
+  // TODO: base64 package can be removed soon as Hermes is adding support for these in 0.74
+  // https://github.com/facebook/hermes/issues/1178
+  headers.append("Authorization", `Basic ${base64.encode(`${clientID}:${clientSecret}`)}`);
+
+  const bodyParams = `grant_type=refresh_token&refresh_token=${token.refresh_token}`;
+
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: headers,
+    body: bodyParams,
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch("https://www.bungie.net/platform/app/oauth/token/", requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          console.error(response);
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function processInitialAuthJWT(jwtToken: object): string {
   try {
-    const result = v.parse(authJWTSchema, jwtToken);
-    return result.membership_id;
+    const initialToken = v.parse(refreshTokenSchema, jwtToken);
+
+    let token: RefreshToken;
+
+    getAccessToken(initialToken)
+      .then((newToken) => {
+        token = v.parse(refreshTokenSchema, newToken);
+        token.time_stamp = new Date().toISOString();
+        const tokenTime = new Date(token.time_stamp).getTime();
+        // pause for 10 seconds, but don't do anything else
+        setTimeout(() => {
+          const timeNow = new Date().getTime();
+          console.log("token time", Math.trunc((timeNow - tokenTime) / 1000));
+        }, 10000);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    return initialToken.membership_id;
   } catch (error) {
     console.error(error);
     return "";
--- native_gg/src/constants/env.ts
+++ native_gg/src/constants/env.ts
@@ -1,11 +1,11 @@
 import { Platform } from "react-native";
 
 // @ts-ignore
-export const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+export const apiKey: string = process.env.EXPO_PUBLIC_API_KEY;
 // @ts-ignore
-export const clientID = process.env.EXPO_PUBLIC_CLIENT_ID;
+export const clientID: string = process.env.EXPO_PUBLIC_CLIENT_ID;
 // @ts-ignore
-export const clientSecret = process.env.EXPO_PUBLIC_CLIENT_SECRET;
+export const clientSecret: string = process.env.EXPO_PUBLIC_CLIENT_SECRET;
 
 const isLocalWeb = process.env.NODE_ENV === "development" && Platform.OS === "web";
 
