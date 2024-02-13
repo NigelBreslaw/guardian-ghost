e6b345f5b66777dcd5592f11f03c8e8c96b8e6a5
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-13 16:21:35 +0200

    Add some types

--- native_gg/src/authentication/bungie/User.ts
+++ native_gg/src/authentication/bungie/User.ts
@@ -1,46 +1,11 @@
 import { apiKey } from "../../constants/env.ts";
-import AuthService from "../AuthService.ts";
 
-// static async function getLinkedProfilesGetRequest( String bungieMembershipId, Bool getAll ) -> Http.Request {
-
-//     var isDesktop = @device(Desktop) || App.isPreview
-//     var apiKey = isDesktop ? Network.Authentication.Secret.desktopApiKey : Network.Authentication.Secret.mobileApiKey
-
-//     ///  Create the request and populate the headers correctly
-//     Http.Request req
-
-//     var authAndToken = await Network.Authentication.getAuthToken( "getLinkedProfilesGetRequest" )
-//     req.setHeader( "Authorization", authAndToken.authRequest.header( "Authorization" ) )
-//     req.setHeader( "X-API-Key", apiKey )
-
-//     Dictionary< String, String > parameters = [
-//         "getAllMemberships": "true",
-//     ]
-
-//     if getAll {
-//         for var key in parameters.keys() {
-//             req.setParameter( key, parameters[ key ] )
-//         }
-//     }
-
-//     req.url ="https://www.bungie.net/Platform/Destiny2/254/Profile/{0}/LinkedProfiles/".arg( bungieMembershipId )
-
-//     return req
-// }
-
-export async function getLinkedProfiles(membership_id: string): Promise<JSON> {
-  const authToken = await AuthService.getTokenAsync();
-  if (authToken === null) {
-    return Promise.reject("No auth token");
-  }
-
+export async function getLinkedProfiles(membership_id: string, access_token: string): Promise<JSON> {
   const headers = new Headers();
-  headers.append("Authorization", `Bearer ${authToken.access_token}`);
+  headers.append("Authorization", `Bearer ${access_token}`);
   headers.append("X-API-Key", apiKey);
 
-  const parameters = new URLSearchParams({
-    getAllMemberships: "true",
-  });
+  const parameters = "?getAllMemberships=true";
 
   console.log("parameters", parameters);
 
