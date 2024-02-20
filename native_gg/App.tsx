7bd4e1708f238532c691a621ea7c8fe413241636
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-20 13:51:45 +0200

    Remove Tamagui (#448)
    Remove tamagui dependencies.
    Remove tamagui front end code.

--- native_gg/App.tsx
+++ native_gg/App.tsx
@@ -1,19 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context";
-import { TamaguiProvider, createTamagui } from "@tamagui/core";
-import { config } from "@tamagui/config/v3";
 import { clientID } from "./src/constants/env.ts";
 import Director from "./src/screens/Director.tsx";
-
-// you usually export this from a tamagui.config.ts file
-const tamaguiConfig = createTamagui(config);
-
-// make TypeScript type everything based on your config
-type Conf = typeof tamaguiConfig;
-declare module "@tamagui/core" {
-  interface TamaguiCustomConfig extends Conf {}
-}
 
 const Stack = createNativeStackNavigator();
 
@@ -23,24 +12,22 @@ export default function App() {
   }
 
   return (
-    <TamaguiProvider config={tamaguiConfig}>
-      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Director"
-              component={Director}
-              options={{
-                title: "",
-                headerTransparent: true,
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </TamaguiProvider>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Director"
+            component={Director}
+            options={{
+              title: "",
+              headerTransparent: true,
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
