6e601bf640d72fe001ed0fdfa9da4b35bf9ad5a4
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-02-29 16:56:36 +0200

    Login button should be at the bottom

--- native/app/App.tsx
+++ native/app/App.tsx
@@ -1,6 +1,5 @@
 import { PaperProvider } from "react-native-paper";
 import GlobalStateProvider from "@/state/GlobalState.tsx";
-import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context";
 import RootScreen from "@/RootScreen.tsx";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -8,11 +7,9 @@ export default function App() {
   return (
     <PaperProvider>
       <GlobalStateProvider>
-        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-          <NavigationContainer>
-            <RootScreen />
-          </NavigationContainer>
-        </SafeAreaProvider>
+        <NavigationContainer>
+          <RootScreen />
+        </NavigationContainer>
       </GlobalStateProvider>
     </PaperProvider>
   );
