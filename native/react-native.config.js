5f97fd01f1ebe615d1ad796904baf5b8f5ccae5a
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-01 10:43:17 +0200

    Add react legacy component names (#614)

---
+++ native/react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  project: {
+    ios: {
+      unstable_reactLegacyComponentNames: ["react-native-fast-image", "CellContainer", "AutoLayoutView"],
+    },
+    android: {},
+  },
+};
