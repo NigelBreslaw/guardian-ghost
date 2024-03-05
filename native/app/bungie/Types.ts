b6bd985d68089fe5c426ee337be223f5e8f84e84
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-05 23:48:01 +0200

    feat: Validate the itemComponents (#670)
    These store useful info such as weapon and armor primaryStat (light level), weapon and armour energy type and more.

--- native/app/bungie/Types.ts
+++ native/app/bungie/Types.ts
@@ -195,7 +195,40 @@ export const getProfileSchema = merge([
       characters: object({
         data: record(string(), CharactersSchema),
       }),
-      itemComponents: object({}),
+
+      itemComponents: object({
+        instances: object({
+          data: record(
+            string(),
+            object({
+              canEquip: boolean(),
+              cannotEquipReason: number(),
+              damageType: number(),
+              damageTypeHash: optional(number()),
+              energy: optional(
+                object({
+                  energyCapacity: number(),
+                  energyType: number(),
+                  energyTypeHash: number(),
+                  energyUnused: number(),
+                  energyUsed: number(),
+                }),
+              ),
+              equipRequiredLevel: number(),
+              isEquipped: boolean(),
+              itemLevel: number(),
+              primaryStat: optional(
+                object({
+                  statHash: number(),
+                  value: number(),
+                }),
+              ),
+              quality: number(),
+              unlockHashesRequiredToEquip: array(number()),
+            }),
+          ),
+        }),
+      }),
       profile: object({}),
       profileCurrencies: object({}),
       profileInventory: object({
