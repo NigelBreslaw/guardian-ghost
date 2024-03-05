52934c3fb3f5f3c69d48126f718e1bd969fdab5f
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-05 17:12:45 +0200

    Refactor Inventory Screen. (#668)
    Tidy up the messy code into seperate components that share the right data.

---
+++ native/app/inventory/UiDataBuilder.ts
@@ -0,0 +1,103 @@
+import type { CharacterGear } from "@/app/bungie/Types.ts";
+import DataService from "@/app/core/DataService.ts";
+import {
+  weaponsPageBuckets,
+  type UiRow,
+  type HeaderRow,
+  UiRowType,
+  type DestinyIconData,
+  type CharacterInventoryRow,
+} from "@/app/inventory/Common";
+
+export function buildUIData(): Array<Array<UiRow>> {
+  const p1 = performance.now();
+  const characterDataArray: Array<Array<UiRow>> = [];
+
+  for (const character in DataService.charactersAndVault.characters) {
+    const characterData = DataService.charactersAndVault.characters[character];
+    const dataArray: Array<UiRow> = [];
+
+    for (const bucket of weaponsPageBuckets) {
+      const header: HeaderRow = {
+        id: `${bucket}_header`,
+        type: UiRowType.Header,
+      };
+      dataArray.push(header);
+      const bucketItems = characterData.items[bucket];
+
+      const equipItem = returnEquippedData(bucketItems);
+      const inventoryRowData0 = returnInventoryRow(bucketItems, 0);
+      const equippedRow = {
+        id: `${bucket}_equipped`,
+        equipped: equipItem,
+        inventory: inventoryRowData0,
+        type: UiRowType.CharacterEquipped,
+      };
+      dataArray.push(equippedRow);
+
+      const inventoryRow1Data = returnInventoryRow(bucketItems, 1);
+      const inventoryRow1: CharacterInventoryRow = {
+        id: `${bucket}_row1`,
+        inventory: inventoryRow1Data,
+        type: UiRowType.CharacterInventory,
+      };
+      dataArray.push(inventoryRow1);
+
+      const inventoryRow2Data = returnInventoryRow(bucketItems, 2);
+      const inventoryRow2: CharacterInventoryRow = {
+        id: `${bucket}_row2`,
+        inventory: inventoryRow2Data,
+        type: UiRowType.CharacterInventory,
+      };
+      dataArray.push(inventoryRow2);
+    }
+    characterDataArray.push(dataArray);
+  }
+  const p2 = performance.now();
+  console.log("buildUIData took:", (p2 - p1).toFixed(4), "ms");
+  return characterDataArray;
+}
+
+function returnEquippedData(characterGear: CharacterGear): DestinyIconData | null {
+  const equipped = characterGear.equipped;
+  if (equipped) {
+    const definition = DataService.itemDefinition.items[equipped.itemHash];
+
+    const iconData: DestinyIconData = {
+      itemHash: equipped.itemHash,
+      itemInstanceId: equipped.itemInstanceId,
+      icon: definition.i,
+    };
+    return iconData;
+  }
+  return null;
+}
+
+function returnInventoryRow(characterGear: CharacterGear, column: number, rowWidth = 3): Array<DestinyIconData> {
+  const rowData: Array<DestinyIconData> = [];
+
+  const startIndex = column * rowWidth;
+  const endIndex = startIndex + rowWidth;
+
+  for (let i = startIndex; i < endIndex; i++) {
+    const item = characterGear.inventory[i];
+    if (item) {
+      const definition = DataService.itemDefinition.items[item.itemHash];
+
+      const iconData: DestinyIconData = {
+        itemHash: item.itemHash,
+        itemInstanceId: item.itemInstanceId,
+        icon: definition.i,
+      };
+      rowData.push(iconData);
+    } else {
+      const frame: DestinyIconData = {
+        itemHash: -1,
+        icon: "",
+      };
+      rowData.push(frame);
+    }
+  }
+
+  return rowData;
+}
---
+++ native/app/inventory/UiRowRenderItem.tsx
@@ -0,0 +1,110 @@
+import DestinyCell from "@/app/inventory/DestinyCell.tsx";
+import EmptyCell from "@/app/inventory/EmptyCell.tsx";
+import {
+  UiRowType,
+  type UiRow,
+  type HeaderRow,
+  type CharacterEquippedRow,
+  type CharacterInventoryRow,
+  ITEM_SIZE,
+} from "@/app/inventory/Common.ts";
+import { useMemo } from "react";
+import { View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  header: {
+    height: 45,
+  },
+  item: {
+    width: 380,
+    height: ITEM_SIZE,
+    paddingLeft: 20,
+    paddingTop: 10,
+    flexDirection: "row",
+  },
+  sectionEquipped: {
+    flex: 1,
+    backgroundColor: "transparent",
+    alignItems: "center",
+  },
+  sectionInventory: {
+    flex: 3,
+    justifyContent: "space-around",
+    flexDirection: "row",
+  },
+});
+
+function HeaderRowUiItem(itemData: HeaderRow) {
+  const data = useMemo(() => {
+    return itemData;
+  }, [itemData]);
+
+  return <View key={itemData.id} style={styles.header} />;
+}
+function EquippedRowUiItem(itemData: CharacterEquippedRow) {
+  const data = useMemo(() => {
+    return itemData;
+  }, [itemData]);
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.sectionEquipped}>
+        <DestinyCell
+          iconUri={`https://www.bungie.net/common/destiny2_content/icons/${data.equipped.icon}`}
+          versionUri="https://www.bungie.net/common/destiny2_content/icons/1b6c8b94cec61ea42edb1e2cb6b45a31.png"
+        />
+      </View>
+      <View style={styles.sectionInventory}>
+        {data.inventory.map((item) => {
+          if (item.itemHash !== -1) {
+            return (
+              <DestinyCell
+                key={item.itemInstanceId}
+                iconUri={`https://www.bungie.net/common/destiny2_content/icons/${item.icon}`}
+                versionUri="https://www.bungie.net/common/destiny2_content/icons/1b6c8b94cec61ea42edb1e2cb6b45a31.png"
+              />
+            );
+          }
+        })}
+      </View>
+    </View>
+  );
+}
+
+function InventoryRowUiItem(itemData: CharacterInventoryRow) {
+  const data = useMemo(() => {
+    return itemData;
+  }, [itemData]);
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.sectionEquipped} />
+      <View style={styles.sectionInventory}>
+        {data.inventory.map((item) => {
+          if (item.itemHash !== -1) {
+            return (
+              <DestinyCell
+                key={item.itemInstanceId}
+                iconUri={`https://www.bungie.net/common/destiny2_content/icons/${item.icon}`}
+                versionUri="https://www.bungie.net/common/destiny2_content/icons/1b6c8b94cec61ea42edb1e2cb6b45a31.png"
+              />
+            );
+          }
+          const id = Math.random();
+          return <EmptyCell key={id} />;
+        })}
+      </View>
+    </View>
+  );
+}
+
+export const UiRowRenderItem = ({ item }: { item: UiRow }) => {
+  switch (item.type) {
+    case UiRowType.CharacterEquipped:
+      return <EquippedRowUiItem id={item.id} equipped={item.equipped} inventory={item.inventory} type={item.type} />;
+    case UiRowType.CharacterInventory:
+      return <InventoryRowUiItem id={item.id} inventory={item.inventory} type={item.type} />;
+    case UiRowType.Header:
+      return <HeaderRowUiItem id={item.id} type={item.type} />;
+  }
+};
