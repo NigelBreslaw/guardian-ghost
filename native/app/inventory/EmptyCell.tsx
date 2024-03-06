b2bd0d658241aa3fc28801e63e0dbee30997beac
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-06 17:23:33 +0200

    feature: Make empty items look better (#688)
    The empty items now look closer to the ingame ones.

--- native/app/inventory/EmptyCell.tsx
+++ native/app/inventory/EmptyCell.tsx
@@ -1,23 +1,60 @@
 import { StyleSheet, View } from "react-native";
 
+const ICON_SIZE = 68;
+const BORDER_COLOR = "#555555";
+const BORDER_RADIUS = 10;
+const BORDER_SIZE = 15;
+const BORDER_WIDTH = 3;
+
 const styles = StyleSheet.create({
-  icon: {
-    width: 68,
-    height: 68,
-    borderRadius: 10,
-    borderWidth: 3,
-    borderColor: "#555555",
+  square: {
+    width: ICON_SIZE,
+    height: ICON_SIZE,
+    position: "relative",
   },
-  frameSize: {
-    width: 68,
-    height: 68,
+  corner: {
+    position: "absolute",
+    width: BORDER_SIZE,
+    height: BORDER_SIZE,
+    borderColor: BORDER_COLOR,
+  },
+  topLeft: {
+    top: 0,
+    left: 0,
+    borderTopWidth: BORDER_WIDTH,
+    borderLeftWidth: BORDER_WIDTH,
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
+  topRight: {
+    top: 0,
+    right: 0,
+    borderTopWidth: BORDER_WIDTH,
+    borderRightWidth: BORDER_WIDTH,
+    borderTopRightRadius: BORDER_RADIUS,
+  },
+  bottomLeft: {
+    bottom: 0,
+    left: 0,
+    borderBottomWidth: BORDER_WIDTH,
+    borderLeftWidth: BORDER_WIDTH,
+    borderBottomLeftRadius: BORDER_RADIUS,
+  },
+  bottomRight: {
+    bottom: 0,
+    right: 0,
+    borderBottomWidth: BORDER_WIDTH,
+    borderRightWidth: BORDER_WIDTH,
+    borderBottomRightRadius: BORDER_RADIUS,
   },
 });
 
 export default function EmptyCell() {
   return (
-    <View style={styles.frameSize}>
-      <View style={styles.icon} />
+    <View style={styles.square}>
+      <View style={[styles.corner, styles.topLeft]} />
+      <View style={[styles.corner, styles.topRight]} />
+      <View style={[styles.corner, styles.bottomLeft]} />
+      <View style={[styles.corner, styles.bottomRight]} />
     </View>
   );
 }
