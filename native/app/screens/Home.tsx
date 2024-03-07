0ff4383daaea7fcc0a597cbe6379f4aa985bf68c
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-07 17:19:43 +0200

    feat: Calculate Flashlist length ahead of time.

--- native/app/screens/Home.tsx
+++ native/app/screens/Home.tsx
@@ -6,10 +6,25 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useGlobalStateContext } from "@/app/state/GlobalState.tsx";
 import { buildUIData } from "@/app/inventory/UiDataBuilder.ts";
 import { UiRowRenderItem } from "@/app/inventory/UiRowRenderItem.tsx";
-import { ITEM_SIZE, type UiRow } from "@/app/inventory/Common.ts";
-import { useEffect, useState } from "react";
+import { ITEM_SIZE, UiRowType, type UiRow, HEADER_SIZE } from "@/app/inventory/Common.ts";
+import { useEffect, useRef, useState } from "react";
 
-const p1 = performance.now();
+function getListSizes(UiData: Array<Array<UiRow>>): Array<number> {
+  const listSizes = [];
+  for (const row of UiData) {
+    let size = 0;
+
+    for (const item of row) {
+      if (item.type === UiRowType.Header) {
+        size += HEADER_SIZE;
+      } else {
+        size += ITEM_SIZE;
+      }
+    }
+    listSizes.push(size);
+  }
+  return listSizes;
+}
 
 export default function HomeScreen({ navigation }: { navigation: NavigationProp<ReactNavigation.RootParamList> }) {
   const globalState = useGlobalStateContext();
@@ -18,6 +33,7 @@ export default function HomeScreen({ navigation }: { navigation: NavigationProp<
   const HOME_WIDTH = width;
 
   const [listData, setListData] = useState<Array<Array<UiRow>>>([]);
+  const listSizeRef = useRef<Array<number>>([]);
 
   const homeStyles = StyleSheet.create({
     homeContainer: {
@@ -32,24 +48,32 @@ export default function HomeScreen({ navigation }: { navigation: NavigationProp<
 
   useEffect(() => {
     if (globalState.dataIsReady) {
+      const UiData = buildUIData();
+      listSizeRef.current = [];
+      listSizeRef.current = getListSizes(UiData);
       setListData(buildUIData());
     }
   }, [globalState.dataIsReady]);
 
   return (
     <ScrollView removeClippedSubviews={true} horizontal pagingEnabled style={homeStyles.homeContainer}>
-      {listData.map((dataArray, index) => (
-        // biome-ignore lint/suspicious/noArrayIndexKey: <Index is unique for each page in this case>
-        <View key={index} style={[homeStyles.page]}>
-          <FlashList
-            estimatedItemSize={ITEM_SIZE}
-            data={dataArray}
-            renderItem={UiRowRenderItem}
-            keyExtractor={(item) => item.id}
-            getItemType={(item) => item.type}
-          />
-        </View>
-      ))}
+      {listData.map((dataArray, index) => {
+        const listSize = listSizeRef.current[index] || 2000;
+        return (
+          // biome-ignore lint/suspicious/noArrayIndexKey: <Index is unique for each page in this case>
+          <View key={index} style={[homeStyles.page]}>
+            <FlashList
+              estimatedItemSize={ITEM_SIZE}
+              data={dataArray}
+              renderItem={UiRowRenderItem}
+              keyExtractor={(item) => item.id}
+              getItemType={(item) => item.type}
+              estimatedListSize={{ width: HOME_WIDTH, height: listSize }}
+              drawDistance={2000}
+            />
+          </View>
+        );
+      })}
     </ScrollView>
   );
 }
