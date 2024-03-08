257dfc519c225902b6542b65ddb9ef87d1e33700
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-09 00:44:44 +0200

    Fix white background flash on android

--- native/app/App.tsx
+++ native/app/App.tsx
@@ -3,11 +3,24 @@ import GlobalStateProvider from "@/state/GlobalState.tsx";
 import RootScreen from "@/RootScreen.tsx";
 import { NavigationContainer } from "@react-navigation/native";
 
+// If the them is not set a white background keeps showing during screen rotation
 export default function App() {
   return (
     <PaperProvider>
       <GlobalStateProvider>
-        <NavigationContainer>
+        <NavigationContainer
+          theme={{
+            colors: {
+              primary: "#17101F",
+              background: "#17101F",
+              card: "#17101F",
+              text: "#17101F",
+              border: "#17101F",
+              notification: "#17101F",
+            },
+            dark: false,
+          }}
+        >
           <RootScreen />
         </NavigationContainer>
       </GlobalStateProvider>
