8744136ab6cb77530d06266c78aabca82be7654d
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-13 18:32:58 +0200

    chore: correctly setup sentry for root component (#801)

--- native/app/App.tsx
+++ native/app/App.tsx
@@ -1,17 +1,10 @@
-import * as Sentry from "@sentry/react-native";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-gesture-handler"; // Avoid crash in production https://reactnavigation.org/docs/stack-navigator/#installation
+SplashScreen.preventAutoHideAsync();
 import RootScreen from "@/RootScreen.tsx";
 import GlobalStateProvider from "@/state/GlobalState.tsx";
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
-
-Sentry.init({
-  dsn: "https://7db2c06ee6ea56cae40a5bd963bad76b:7d7182201a659982c7b30a4512d4ad7a@o4506899216728065.ingest.us.sentry.io/4506899221970944",
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 // If the them is not set a white background keeps showing during screen rotation
 function App() {
@@ -38,4 +31,4 @@ function App() {
   );
 }
 
-export default Sentry.wrap(App);
+export default App;
