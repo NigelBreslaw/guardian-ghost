ba1b0d9d4887484d5f2f8c38a421be5f21236a31
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-17 20:04:21 +0200

    refactor: delete dead code and move code to 'helpers.ts'

--- native/app/utilities/Helpers.ts
+++ native/app/utilities/Helpers.ts
@@ -11,3 +11,29 @@ export const debounce = (func: Function, delay = 0) => {
     }, delay);
   };
 };
+
+import StorageGG from "@/storage/StorageGG";
+
+export function getCustomItemDefinition(language = "en"): Promise<JSON> {
+  const requestOptions: RequestInit = {
+    method: "GET",
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(`https://app.guardianghost.com/json/${language}.json`, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          console.error(response);
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        console.error("Failed to download custom Item Definition", error);
+        reject(error);
+      });
+  });
+}
