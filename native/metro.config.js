13c5779ff40833f53f91f0661e684d9d8e3e29f3
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-23 14:50:01 +0200

    chore: Stop watchman errors (#922)

--- native/metro.config.js
+++ native/metro.config.js
@@ -1,7 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
+const projectRoot = __dirname;
+
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getSentryExpoConfig(__dirname);
+const config = getSentryExpoConfig(projectRoot);
+
+config.watchFolders = [projectRoot];
 
 module.exports = config;
