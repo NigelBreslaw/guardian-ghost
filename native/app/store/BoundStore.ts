61d7c49ea87d49ba5f3a2ad650c2d8bd680de78f
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-24 11:56:08 +0200

    Convert all stores to slices

--- native/app/store/BoundStore.ts
+++ native/app/store/BoundStore.ts
@@ -1,10 +1,16 @@
 import { create } from "zustand";
 import { createAccountSlice, type AccountSlice } from "./AccountSlice.ts";
-import { createAuthenticationSlice, type AuthenticationSlice } from "@/app/store/AuthenticationSlice.ts";
+import { createAuthenticationSlice, type AuthenticationSlice } from "./AuthenticationSlice.ts";
+import { createDefinitionsSlice, type DefinitionsSlice } from "./DefinitionsSlice.ts";
+import { createGlobalStateSlice, type GlobalStateSlice } from "./GlobalStateSlice.ts";
+import { createInventorySlice, type InventorySlice } from "./InventorySlice.ts";
 
-interface IStore extends AccountSlice, AuthenticationSlice {}
+interface IStore extends AccountSlice, AuthenticationSlice, DefinitionsSlice, GlobalStateSlice, InventorySlice {}
 
 export const useBoundStore = create<IStore>((...a) => ({
   ...createAccountSlice(...a),
   ...createAuthenticationSlice(...a),
+  ...createDefinitionsSlice(...a),
+  ...createGlobalStateSlice(...a),
+  ...createInventorySlice(...a),
 }));
