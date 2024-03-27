c4c4bd09e9124163855960d4ab3184abd02b6200
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-27 10:11:32 +0200

    refactor: Reduce import cycle (#1000)
    * Move some state around
    
    * Dismiss the splash screen via store to not cause an app re-render

--- native/app/store/AuthenticationLogic.ts
+++ native/app/store/AuthenticationLogic.ts
@@ -1,5 +1,5 @@
 import { BungieUserSchema, type BungieUser, linkedProfilesSchema } from "@/app/bungie/Types.ts";
-import { useGGStore } from "@/app/store/GGStore.ts";
+// import { useGGStore } from "@/app/store/GGStore.ts";
 import {
   authTokenSchema,
   isValidAccessToken,
@@ -10,7 +10,7 @@ import {
 } from "@/app/store/Utilities.ts";
 import { Store } from "@/constants/storage.ts";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { object, parse, safeParse, string } from "valibot";
+import { object, safeParse, string } from "valibot";
 import { parse as linkingParse } from "expo-linking";
 import { getBungieUser, getLinkedProfiles } from "@/app/bungie/Account.ts";
 import { randomUUID } from "expo-crypto";
@@ -18,60 +18,46 @@ import * as WebBrowser from "expo-web-browser";
 import { clientID, redirectURL } from "@/constants/env.ts";
 import { Platform } from "react-native";
 
-let currentUserID = "";
 const queue: (() => Promise<void>)[] = [];
 let isProcessing = false;
-let authToken: AuthToken | null = null;
 let stateID = "";
 const usedAuthCodes: Array<string> = [];
 
-export async function initAuthentication(): Promise<void> {
-  try {
-    const p1 = performance.now();
-
-    const savedAccount = await AsyncStorage.getItem(Store._bungie_user);
-    if (savedAccount) {
-      const validatedAccount = parse(BungieUserSchema, JSON.parse(savedAccount));
-      setCurrentAccount(validatedAccount);
-    } else {
-      return;
+export async function loadBungieUser(): Promise<BungieUser | null> {
+  const savedAccount = await AsyncStorage.getItem(Store._bungie_user);
+  if (savedAccount) {
+    const validatedAccount = safeParse(BungieUserSchema, JSON.parse(savedAccount));
+    if (validatedAccount.success) {
+      return validatedAccount.output;
     }
-
-    const savedToken = await AsyncStorage.getItem(`${currentUserID}${Store._refresh_token}`);
-    if (savedToken) {
-      const tokenParse = safeParse(authTokenSchema, JSON.parse(savedToken));
-      if (tokenParse.success) {
-        await validateAndSetToken(tokenParse.output);
-        const p2 = performance.now();
-        console.log("init() took:", (p2 - p1).toFixed(4), "ms");
-        return;
-      }
-    }
+    throw new Error("Validation failed");
     // Logout as the saved token is invalid
-    logoutCurrentUser();
-  } catch (e) {
-    console.error(e);
+    // logoutCurrentUser();
   }
+  throw new Error("No saved account found");
 }
 
-function setCurrentAccount(bungieUser: BungieUser | null) {
-  if (bungieUser) {
-    currentUserID = bungieUser.profile.membershipId;
-  } else {
-    currentUserID = "";
-  }
+export async function loadToken(membershipId: string): Promise<AuthToken | null> {
+  const savedToken = await AsyncStorage.getItem(`${membershipId}${Store._refresh_token}`);
+  if (savedToken) {
+    const tokenParse = safeParse(authTokenSchema, JSON.parse(savedToken));
 
-  useGGStore.getState().setBungieUser(bungieUser);
+    if (tokenParse.success) {
+      return tokenParse.output;
+    }
+    throw new Error("Validation failed");
+  }
+  throw new Error("No saved token found");
 }
 
-function validateAndSetToken(token: AuthToken): Promise<boolean> {
+function validateAndSetToken(token: AuthToken, membershipId: string): Promise<boolean> {
   return new Promise((resolve, reject) => {
     const isValidRefresh = isValidRefreshToken(token);
     if (!isValidRefresh) {
       // Nothing can be done. The user needs to re-auth.
       // TODO: Log out the user
       console.error("Refresh token expired");
-      logoutCurrentUser();
+      logoutCurrentUser(membershipId);
       return reject(false);
     }
 
@@ -84,15 +70,13 @@ function validateAndSetToken(token: AuthToken): Promise<boolean> {
 
           if (parsedToken.success) {
             console.info("Retrieved new token");
-            saveAndSetToken(parsedToken.output);
-            useGGStore.getState().setSystemDisabled(false);
+            saveAndSetToken(parsedToken.output, membershipId);
             return resolve(true);
           }
 
           const parsedError = safeParse(object({ error: string(), error_description: string() }), newAuthToken);
           if (parsedError.success && parsedError.output.error_description === "SystemDisabled") {
             console.warn("System disabled");
-            useGGStore.getState().setSystemDisabled(true);
             return resolve(true);
           }
           // Don't log the user out, but maybe show an error and give them a chance to logout and back in again?
@@ -104,53 +88,40 @@ function validateAndSetToken(token: AuthToken): Promise<boolean> {
         });
     } else {
       console.info("Access token valid");
-      saveAndSetToken(token);
+      saveAndSetToken(token, membershipId);
 
       return resolve(true);
     }
   });
 }
 
-function saveAndSetToken(token: AuthToken) {
+// TODO: Async and needs error handling?
+function saveAndSetToken(token: AuthToken, membershipId: string) {
+  if (membershipId === "") {
+    console.error("No membershipId !!!!!");
+    return;
+  }
+
   const parsedToken = safeParse(authTokenSchema, token);
 
   if (parsedToken.success) {
-    if (currentUserID === "") {
-      console.error("No currentUserID!!!");
-    } else {
-      AsyncStorage.setItem(`${currentUserID}${Store._refresh_token}`, JSON.stringify(token));
-      setAuthToken(token);
-    }
-  } else {
-    console.error("Failed to save token. Token is invalid");
+    AsyncStorage.setItem(`${membershipId}${Store._refresh_token}`, JSON.stringify(token));
+    // setAuthToken(token);
   }
 }
 
-function setAuthToken(token: AuthToken | null) {
-  authToken = token;
-
-  const authenticated = isAuthenticated();
-  if (authenticated) {
-    useGGStore.getState().setAuthenticated();
-  } else {
-    useGGStore.getState().setNotAuthenticated();
-  }
-}
-
-// Method to check if user data and auth token exist
-export function isAuthenticated(): boolean {
-  // TODO: This is wrong. It should use getTokenAsync so it needs to be async and whatever calls it should await.
-  return authToken ? true : false;
-}
-
-export function getTokenAsync(errorMessage: string): Promise<AuthToken | null> {
+export function getTokenAsync(
+  authToken: AuthToken | null,
+  membershipId: string,
+  errorMessage: string,
+): Promise<AuthToken | null> {
   return new Promise((resolve, reject) => {
     // Function to add a new request to the queue
     const enqueue = () => {
       console.log("getTokenAsync queue length:", queue.length);
       queue.push(async () => {
         try {
-          const result = await getTokenInternal(errorMessage);
+          const result = await getTokenInternal(authToken, membershipId, errorMessage);
 
           resolve(result);
         } catch {
@@ -178,10 +149,14 @@ export function getTokenAsync(errorMessage: string): Promise<AuthToken | null> {
   });
 }
 
-function getTokenInternal(errorMessage: string): Promise<AuthToken | null> {
+function getTokenInternal(
+  authToken: AuthToken | null,
+  membershipId: string,
+  errorMessage: string,
+): Promise<AuthToken | null> {
   return new Promise((resolve, reject) => {
     if (authToken) {
-      validateAndSetToken(authToken)
+      validateAndSetToken(authToken, membershipId)
         .then(() => {
           const token = authToken;
           if (token) {
@@ -201,14 +176,13 @@ function getTokenInternal(errorMessage: string): Promise<AuthToken | null> {
   });
 }
 
-export async function logoutCurrentUser() {
-  console.info("logoutCurrentUser");
+export async function logoutCurrentUser(membershipId: string) {
+  if (membershipId === "") {
+    console.error("No membershipId !!!!!");
+  }
   try {
-    useGGStore.getState().setNotAuthenticated();
     await AsyncStorage.removeItem(Store._bungie_user);
-    await AsyncStorage.removeItem(`${currentUserID}${Store._refresh_token}`);
-    authToken = null;
-    currentUserID = "";
+    await AsyncStorage.removeItem(`${membershipId}${Store._refresh_token}`);
   } catch {
     throw new Error("Error removing current user from storage");
   }
@@ -264,21 +238,20 @@ async function buildBungieAccount(authToken: AuthToken) {
 
     if (parsedProfiles.success) {
       const bungieUser = getBungieUser(parsedProfiles.output);
-      setCurrentAccount(bungieUser);
+      // setCurrentAccount(bungieUser);
       await AsyncStorage.setItem(Store._bungie_user, JSON.stringify(bungieUser));
-      saveAndSetToken(authToken);
-      useGGStore.getState().setLoggingIn(false);
+      saveAndSetToken(authToken, bungieUser.profile.membershipId);
+      // useGGStore.getState().setLoggingIn(false);
       return;
     }
     // This is a catastrophic failure. The user is logged in but we can't get their linked profiles.
     // It needs some kind of big alert and then a logout.
     console.error("Error in buildBungieAccount", parsedProfiles.output);
-    useGGStore.getState().setLoggingIn(false);
+    // useGGStore.getState().setLoggingIn(false);
   }
 }
 
 export function startAuth(): void {
-  useGGStore.getState().setLoggingIn(true);
   stateID = randomUUID();
   const authURL = `https://www.bungie.net/en/oauth/authorize?client_id=${clientID}&response_type=code&reauth=true&state=${stateID}`;
 
@@ -292,12 +265,10 @@ export function startAuth(): void {
         if (Platform.OS === "android") {
           // User probably went back from the login webview without completing auth flow
           console.info("Failed to complete auth session", result);
-          useGGStore.getState().setLoggingIn(false);
         }
       } else {
         // Used for all platforms
         console.info("Failed to open auth session", result);
-        useGGStore.getState().setLoggingIn(false);
       }
     })
     .catch((e) => {
--- native/app/store/GlobalStateSlice.ts
+++ native/app/store/GlobalStateSlice.ts
@@ -3,13 +3,9 @@ import type { StateCreator } from "zustand";
 export interface GlobalStateSlice {
   refreshing: boolean;
   setRefreshing: (refreshing: boolean) => void;
-  systemDisabled: boolean;
-  setSystemDisabled: (systemDisabled: boolean) => void;
 }
 
 export const createGlobalStateSlice: StateCreator<GlobalStateSlice> = (set) => ({
   refreshing: false,
   setRefreshing: (refreshing) => set({ refreshing }),
-  systemDisabled: false,
-  setSystemDisabled: (systemDisabled) => set({ systemDisabled }),
 });
