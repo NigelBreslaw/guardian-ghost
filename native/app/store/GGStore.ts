99541e9dea08614e33fd7017b863ee7d52cda0ca
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-28 09:53:28 +0200

    refactor(state): No need for GlobalState.ts

--- native/app/store/GGStore.ts
+++ native/app/store/GGStore.ts
@@ -2,13 +2,11 @@ import { create } from "zustand";
 import { type AccountSlice, createAccountSlice } from "./AccountSlice.ts";
 import { type AuthenticationSlice, createAuthenticationSlice } from "./AuthenticationSlice.ts";
 import { type DefinitionsSlice, createDefinitionsSlice } from "./DefinitionsSlice.ts";
-import { type GlobalStateSlice, createGlobalStateSlice } from "./GlobalStateSlice.ts";
 
-interface IStore extends AccountSlice, AuthenticationSlice, DefinitionsSlice, GlobalStateSlice {}
+interface IStore extends AccountSlice, AuthenticationSlice, DefinitionsSlice {}
 
 export const useGGStore = create<IStore>()((...a) => ({
   ...createAccountSlice(...a),
   ...createAuthenticationSlice(...a),
   ...createDefinitionsSlice(...a),
-  ...createGlobalStateSlice(...a),
 }));
