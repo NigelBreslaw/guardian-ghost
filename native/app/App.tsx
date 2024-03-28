0204383c16c8c2068b28b87382a6fcb4103ae43e
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-03-28 17:14:34 +0200

    refactor: merge RootStack into App.tsx (#1023)

--- native/app/App.tsx
+++ native/app/App.tsx
@@ -1,31 +1,84 @@
 import * as SplashScreen from "expo-splash-screen";
 import "react-native-gesture-handler"; // Avoid crash in production https://reactnavigation.org/docs/stack-navigator/#installation
-import RootScreen from "@/RootScreen.tsx";
 import { useGGStore } from "@/app/store/GGStore.ts";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, type Theme } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useEffect } from "react";
+import { getFullProfile } from "@/app/bungie/BungieApi.ts";
+import MainDrawer from "@/app/screens/MainDrawer.tsx";
+import Login from "@/app/screens/Login.tsx";
+import { Platform } from "react-native";
+import BottomSheet from "@/app/screens/BottomSheet.tsx";
+export type RootStackParamList = {
+  Login: undefined;
+  Root: undefined;
+  BottomSheet: { itemInstanceId: string | undefined; itemHash: number };
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
 
 SplashScreen.preventAutoHideAsync();
 useGGStore.getState().initDefinitions();
 useGGStore.getState().initAuthentication();
+
+const navigationContainerTheme: Theme = {
+  colors: {
+    primary: "#17101F",
+    background: "#17101F",
+    card: "#17101F",
+    text: "#17101F",
+    border: "#17101F",
+    notification: "#17101F",
+  },
+  dark: false,
+};
 // If the them is not set a white background keeps showing during screen rotation
 function App() {
+  const definitionsReady = useGGStore((state) => state.definitionsReady);
+  const authenticated = useGGStore((state) => state.authenticated);
+
+  useEffect(() => {
+    if (authenticated === "AUTHENTICATED" && definitionsReady) {
+      console.log("trigger: download getProfile()");
+      getFullProfile();
+    }
+  }, [authenticated, definitionsReady]);
+
   return (
     <PaperProvider>
-      <NavigationContainer
-        theme={{
-          colors: {
-            primary: "#17101F",
-            background: "#17101F",
-            card: "#17101F",
-            text: "#17101F",
-            border: "#17101F",
-            notification: "#17101F",
-          },
-          dark: false,
-        }}
-      >
-        <RootScreen />
+      <NavigationContainer theme={navigationContainerTheme}>
+        <RootStack.Navigator>
+          <RootStack.Group>
+            <RootStack.Screen
+              name="Root"
+              component={MainDrawer}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </RootStack.Group>
+          <RootStack.Group screenOptions={{ presentation: "modal", gestureEnabled: false, headerShown: false }}>
+            <RootStack.Screen name="Login" component={Login} options={{ title: "Login" }} />
+          </RootStack.Group>
+          <RootStack.Group
+            screenOptions={{
+              presentation: Platform.OS === "ios" ? "modal" : "transparentModal",
+              headerShown: false,
+              cardStyle: {
+                backgroundColor: "transparent",
+              },
+            }}
+          >
+            <RootStack.Screen name="BottomSheet" component={BottomSheet} initialParams={{ itemInstanceId: "" }} />
+          </RootStack.Group>
+        </RootStack.Navigator>
       </NavigationContainer>
     </PaperProvider>
   );
