3f944ff4c0f506dc404810fa2b0e24bc21c54ef0
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-08 09:51:11 +0300

    feature: Simple InventoryHeader (#1143)

---
+++ native/app/screens/InventoryHeader.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import { Image } from "expo-image";
+import { useGGStore } from "@/app/store/GGStore.ts";
+
+function InventoryHeader() {
+  const currentListIndex = useGGStore((state) => state.currentListIndex);
+  const characterBackgroundEmblem = useGGStore((state) => state.ggCharacters[currentListIndex]?.emblemBackgroundPath);
+  return (
+    <View style={{ flex: 1 }}>
+      <Image style={{ flex: 1 }} transition={150} source={{ uri: characterBackgroundEmblem }} />
+    </View>
+  );
+}
+
+export default InventoryHeader;
