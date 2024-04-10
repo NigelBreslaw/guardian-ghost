d1bdfc1114c9b374363e84b35be995947b3599e4
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-10 14:04:30 +0300

    refactor: simplify UiCell

--- native/app/inventory/UiRowRenderItem.tsx
+++ native/app/inventory/UiRowRenderItem.tsx
@@ -1,6 +1,4 @@
-import BlankCell from "@/app/inventory/BlankCell.tsx";
 import { type UiCell, UiCellType } from "@/app/inventory/Common.ts";
-import EmptyCell from "@/app/inventory/EmptyCell.tsx";
 import EquipSection from "@/app/inventory/EquipSection.tsx";
 import SeparatorRow from "@/app/inventory/SeparatorRow";
 import Vault5x5Cell from "@/app/inventory/VaultSection5x5.tsx";
@@ -10,12 +8,6 @@ export const UiCellRenderItem = ({ item }: { item: UiCell }) => {
   switch (item.type) {
     case UiCellType.Separator:
       return <SeparatorRow />;
-    case UiCellType.EmptyCell:
-      return <EmptyCell />;
-    case UiCellType.BlankCell:
-      return <BlankCell />;
-    case UiCellType.DestinyCell:
-      return <BlankCell />;
     case UiCellType.EquipSectionCell:
       return <EquipSection data={item} />;
     case UiCellType.Vault5x5Cell:
