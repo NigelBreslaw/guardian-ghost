1b87b741eb0fbc8c94b7a638fb1f339fd81396dc
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-12 10:57:15 +0300

    fix: Items would transfer to the vault but not be visible (#1216)

--- native/app/bungie/Hashes.ts
+++ native/app/bungie/Hashes.ts
@@ -1,23 +1,3 @@
-export const characterBuckets = [
-  1498876634, // kinetic weapons
-  2465295065, // energy weapons
-  953998645, // power weapons
-  3448274439, // helmet
-  3551918588, // gauntlets
-  14239492, // chest armor
-  20886954, // leg armor
-  1585787867, // class armor
-  4023194814, // ghost
-  2025709351, // vehicle
-  284967655, // ships
-  3284755031, // subclass
-  4292445962, // banners
-  4274335291, // emblems
-  3683254069, // finisher
-  1107761855, // emotes
-  1506418338, // artifact
-];
-
 export enum GuardianClassType {
   Titan = 0,
   Hunter = 1,
