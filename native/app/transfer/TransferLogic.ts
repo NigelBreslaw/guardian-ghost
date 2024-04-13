a2001ea638cceed2955350cada7fb7b4f48e2abe
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-13 23:32:56 +0300

    Lint fix

--- native/app/transfer/TransferLogic.ts
+++ native/app/transfer/TransferLogic.ts
@@ -2,11 +2,7 @@ import type { DestinyItem } from "@/app/bungie/Types.ts";
 import { basePath } from "@/app/inventory/Common.ts";
 import { itemsDefinition } from "@/app/store/Definitions.ts";
 import { useGGStore } from "@/app/store/GGStore.ts";
-import {
-  GLOBAL_CONSUMABLES_CHARACTER_ID,
-  GLOBAL_INVENTORY_NAMES,
-  VAULT_CHARACTER_ID,
-} from "@/app/utilities/Constants.ts";
+import { GLOBAL_INVENTORY_NAMES, VAULT_CHARACTER_ID } from "@/app/utilities/Constants.ts";
 import { apiKey } from "@/constants/env.ts";
 import { number, object, optional, safeParse, string } from "valibot";
 
