b09e3dd4857d55d9df36639d3697e24419e40d2f
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-15 09:19:40 +0300

    feature: UIDataSlice (#1264)
    A place for some semi global UI data properties

---
+++ native/app/store/UIDataSlice.ts
@@ -0,0 +1,17 @@
+import type { IStore } from "@/app/store/GGStore.ts";
+import type { StateCreator } from "zustand";
+
+export type UIDataSliceSetter = Parameters<StateCreator<IStore, [], [], UIDataSlice>>[0];
+export type UiDataSliceGetter = Parameters<StateCreator<IStore, [], [], UIDataSlice>>[1];
+
+export interface UIDataSlice {
+  maxLostItemsColumns: number;
+  setLostItemsColumns: (maxLostItemsColumns: number) => void;
+}
+
+export const createUIDataSlice: StateCreator<IStore, [], [], UIDataSlice> = (set, _get) => ({
+  maxLostItemsColumns: 0,
+  setLostItemsColumns: (maxLostItemsColumns) => {
+    set({ maxLostItemsColumns });
+  },
+});
