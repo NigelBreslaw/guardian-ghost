0b90e35b97726bc00f2980983c4b331a4878726e
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-15 09:18:29 +0300

    feature: UIDataSlice
    A place for some semi global UI data properties

--- native/app/store/GGStore.ts
+++ native/app/store/GGStore.ts
@@ -2,11 +2,13 @@ import { create } from "zustand";
 import { type AccountSlice, createAccountSlice } from "./AccountSlice.ts";
 import { type AuthenticationSlice, createAuthenticationSlice } from "./AuthenticationSlice.ts";
 import { type DefinitionsSlice, createDefinitionsSlice } from "./DefinitionsSlice.ts";
+import { type UIDataSlice, createUIDataSlice } from "./UIDataSlice.ts";
 
-export interface IStore extends AccountSlice, AuthenticationSlice, DefinitionsSlice {}
+export interface IStore extends AccountSlice, AuthenticationSlice, DefinitionsSlice, UIDataSlice {}
 
 export const useGGStore = create<IStore>()((...a) => ({
   ...createAccountSlice(...a),
   ...createAuthenticationSlice(...a),
   ...createDefinitionsSlice(...a),
+  ...createUIDataSlice(...a),
 }));
