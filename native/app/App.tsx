24cab28284765eb1e1ad54690b793dcf62f7cc95
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-21 22:43:36 +0300

    refactor: Auto refresh can be reset more easily
    This allows any part of the app to easily reset the 35 second counter till a refresh happens. Now manual refreshes reset the counter. As does transfering items.

--- native/app/App.tsx
+++ native/app/App.tsx
@@ -69,7 +69,6 @@ function App() {
   const navigationRef = useRef<NavigationContainerRef<ReactNavigation.RootParamList>>(null);
   const { width } = useWindowDimensions();
   const SCREEN_WIDTH = width;
-  const lastRefreshRef = useRef<number>(0);
 
   useEffect(() => {
     if (SCREEN_WIDTH) {
@@ -86,8 +85,7 @@ function App() {
       }
     } else if (authenticated === "AUTHENTICATED" && definitionsReady) {
       getFullProfile();
-      const now = performance.now();
-      lastRefreshRef.current = now;
+      useGGStore.getState().setLastRefreshTime();
       const intervalId = setInterval(refreshIfNeeded, 2000);
 
       return () => clearInterval(intervalId);
@@ -95,11 +93,10 @@ function App() {
   }, [authenticated, definitionsReady]);
 
   function refreshIfNeeded() {
-    const lastRefresh = lastRefreshRef.current;
+    const lastRefresh = useGGStore.getState().lastRefreshTime;
     const now = performance.now();
     if (now - lastRefresh > 35000) {
       getFullProfile();
-      lastRefreshRef.current = now;
     }
   }
 
