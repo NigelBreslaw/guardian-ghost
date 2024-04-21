72e39fe923ee9e2695efbe2db0c9f54c66b4ca83
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-21 18:52:41 +0300

    refactor: better sizes (#1359)
    * refactor: better sizes
    
    * lint

--- native/app/inventory/LostItemsUI.tsx
+++ native/app/inventory/LostItemsUI.tsx
@@ -1,5 +1,5 @@
 import type { DestinyIconData } from "@/app/inventory/Common.ts";
-import { DEFAULT_MARGIN, ICON_MARGIN, ICON_SIZE } from "@/app/utilities/UISize.ts";
+import { DEFAULT_MARGIN, FOOTER_HEIGHT, ICON_MARGIN, ICON_SIZE } from "@/app/utilities/UISize.ts";
 import DestinyCell from "@/app/inventory/DestinyCell.tsx";
 import EmptyCell from "@/app/inventory/EmptyCell.tsx";
 import { useGGStore } from "@/app/store/GGStore.ts";
@@ -15,7 +15,7 @@ function LostItemsUI(props: EngramsProps) {
   const maxLostItemsColumns = useGGStore.getState().maxLostItemsColumns;
   const styles = StyleSheet.create({
     root: {
-      height: (ICON_SIZE + ICON_MARGIN) * maxLostItemsColumns,
+      height: ICON_SIZE * maxLostItemsColumns + (maxLostItemsColumns - 1) * ICON_MARGIN,
       paddingLeft: DEFAULT_MARGIN,
       paddingRight: DEFAULT_MARGIN,
       flex: 5,
@@ -24,30 +24,36 @@ function LostItemsUI(props: EngramsProps) {
       justifyContent: "space-between",
       alignContent: "space-between",
     },
+    footer: {
+      height: FOOTER_HEIGHT,
+    },
   });
   const totalItems = Array.from({ length: 5 * maxLostItemsColumns });
 
   return (
-    <View style={styles.root}>
-      {totalItems.map((_v, index) => {
-        const item = props.data[index];
-        if (item) {
-          if (item.engram) {
+    <View>
+      <View style={styles.root}>
+        {totalItems.map((_v, index) => {
+          const item = props.data[index];
+          if (item) {
+            if (item.engram) {
+              return (
+                // biome-ignore lint/suspicious/noArrayIndexKey: <Index is unique for each page in this case>
+                <EngramCell key={index} data={item} />
+              );
+            }
             return (
-              // biome-ignore lint/suspicious/noArrayIndexKey: <Index is unique for each page in this case>
-              <EngramCell key={index} data={item} />
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              <DestinyCell key={index} data={item} />
             );
           }
           return (
             // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-            <DestinyCell key={index} data={item} />
+            <EmptyCell key={index} />
           );
-        }
-        return (
-          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-          <EmptyCell key={index} />
-        );
-      })}
+        })}
+      </View>
+      <View style={styles.footer} />
     </View>
   );
 }
