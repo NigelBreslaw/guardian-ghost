956e177ee9a4c8bcfa12c604f125be613eea473a
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-04-25 12:40:58 +0300

    refactor: Move TransferEquipButtons into its own file

--- native/app/screens/BottomSheet.tsx
+++ native/app/screens/BottomSheet.tsx
@@ -1,20 +1,14 @@
 import type { DestinyItem } from "@/app/bungie/Types.ts";
+import TransferEquipButtons from "@/app/screens/TransferEquipButtons.tsx";
 import { itemTypeDisplayName, itemsDefinition } from "@/app/store/Definitions.ts";
 import { useGGStore } from "@/app/store/GGStore.ts";
 import { startTransfer } from "@/app/transfer/TransferLogic.ts";
-import { GLOBAL_INVENTORY_NAMES, VAULT_CHARACTER_ID } from "@/app/utilities/Constants.ts";
 import type { NavigationProp, RouteProp } from "@react-navigation/native";
 import { Image } from "expo-image";
 import { useEffect, useRef, useState } from "react";
 import { StatusBar, StyleSheet, Text, View, useWindowDimensions, TextInput, Platform, Keyboard } from "react-native";
-import {
-  Gesture,
-  GestureDetector,
-  GestureHandlerRootView,
-  TouchableWithoutFeedback,
-} from "react-native-gesture-handler";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import RBSheet from "react-native-raw-bottom-sheet";
-import { runOnJS } from "react-native-reanimated";
 
 type ViewData = {
   itemInstanceId: string | undefined;
@@ -50,182 +44,6 @@ function buildViewData(destinyItem: DestinyItem): ViewData {
     itemTypeDisplayName: itd ? itemTypeDisplayName[itd]?.toLocaleUpperCase() ?? "" : "",
   };
   return viewData;
-}
-
-type TransferEquipButtonsProps = {
-  currentCharacterId: string;
-  destinyItem: DestinyItem;
-  close: () => void;
-  startTransfer: (toCharacterId: string, equipOnTarget: boolean) => void;
-};
-
-function TransferEquipButtons(props: TransferEquipButtonsProps) {
-  const rectangles = [];
-
-  const ggCharacters = useGGStore((state) => state.ggCharacters);
-  const scale = 0.6;
-  const originalWidth = 350;
-  const originalHeight = 96;
-  const transferWidth = originalWidth * scale;
-  const transferHeight = originalHeight * scale;
-  const borderRadius = 15;
-
-  const itemDefinition = itemsDefinition[props.destinyItem.itemHash];
-  const nonTransferable = itemDefinition?.nt === 1;
-  const equipable = itemDefinition?.e === 1;
-
-  function calcEquipOpacity(buttonCharacterId: string): number {
-    if (buttonCharacterId === VAULT_CHARACTER_ID) {
-      return 0;
-    }
-    if (!equipable) {
-      return 0;
-    }
-    if (calcEquipButtonDisabled(buttonCharacterId)) {
-      return 0.2;
-    }
-    return 1;
-  }
-
-  function calcTransferOpacity(buttonCharacterId: string): number {
-    if (nonTransferable) {
-      return 0;
-    }
-    if (calcTransferButtonDisabled(buttonCharacterId)) {
-      return 0.2;
-    }
-    return 1;
-  }
-
-  // only call this if you already checked the item is equipable
-  function calcEquipButtonDisabled(buttonCharacterId: string): boolean {
-    // is this item already on the character, but not in the postmaster?
-    if (buttonCharacterId === props.currentCharacterId && props.destinyItem.bucketHash !== 215593132) {
-      if (props.destinyItem.equipped) {
-        return true;
-      }
-      return false;
-    }
-    return false;
-  }
-
-  function calcTransferButtonDisabled(buttonCharacterId: string): boolean {
-    // is this item already on the character, but not in the postmaster?
-    if (buttonCharacterId === props.currentCharacterId && props.destinyItem.bucketHash !== 215593132) {
-      return true;
-    }
-    return false;
-  }
-
-  for (const ggCharacter of ggCharacters) {
-    // Global items can only be transferred to the vault
-    if (GLOBAL_INVENTORY_NAMES.includes(props.destinyItem.characterId)) {
-      if (ggCharacter.characterId !== VAULT_CHARACTER_ID) {
-        continue;
-      }
-    }
-
-    const isTransferDisabled = calcTransferButtonDisabled(ggCharacter.characterId);
-    const isEquipDisabled = calcEquipButtonDisabled(ggCharacter.characterId);
-
-    if (nonTransferable && ggCharacter.characterId !== props.currentCharacterId) {
-      continue;
-    }
-
-    const transferTap = Gesture.Tap().onBegin(() => {
-      if (isTransferDisabled) {
-        return;
-      }
-      runOnJS(props.startTransfer)(ggCharacter.characterId, false);
-      runOnJS(props.close)();
-    });
-    const transferAndEquipTap = Gesture.Tap().onBegin(() => {
-      if (isEquipDisabled) {
-        return;
-      }
-      runOnJS(props.startTransfer)(ggCharacter.characterId, true);
-      runOnJS(props.close)();
-    });
-
-    rectangles.push(
-      <GestureHandlerRootView key={ggCharacter.characterId} style={{ flexDirection: "row", gap: 5 }}>
-        <GestureDetector gesture={transferTap}>
-          <View
-            style={{
-              width: transferWidth,
-              height: transferHeight,
-              borderRadius,
-              overflow: "hidden",
-              opacity: calcTransferOpacity(ggCharacter.characterId),
-              display: calcTransferOpacity(ggCharacter.characterId) === 0 ? "none" : "flex",
-            }}
-          >
-            <View
-              style={{
-                width: originalWidth,
-
-                overflow: "hidden",
-                transformOrigin: "top left",
-                transform: [{ scale: scale }],
-              }}
-            >
-              <Image source={ggCharacter.emblemBackgroundPath} style={{ width: 474, height: 96 }} />
-              <View style={[StyleSheet.absoluteFillObject, { flex: 1, alignContent: "center" }]}>
-                <Text>Character: {ggCharacter.characterId}</Text>
-              </View>
-            </View>
-            <View
-              style={{
-                position: "absolute",
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-                borderRadius,
-                borderWidth: 1,
-                borderColor: "grey",
-              }}
-            />
-          </View>
-        </GestureDetector>
-        <GestureDetector gesture={transferAndEquipTap}>
-          <View
-            style={{
-              width: transferHeight,
-              height: transferHeight,
-              borderRadius,
-              overflow: "hidden",
-              opacity: calcEquipOpacity(ggCharacter.characterId),
-              display: calcTransferOpacity(ggCharacter.characterId) === 0 ? "none" : "flex",
-            }}
-          >
-            <View
-              style={{
-                transformOrigin: "top left",
-                transform: [{ scale: scale }],
-              }}
-            >
-              <Image source={ggCharacter.emblemPath} style={{ width: 96, height: 96 }} />
-            </View>
-            <View
-              style={{
-                position: "absolute",
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-                borderRadius,
-                borderWidth: 1,
-                borderColor: "grey",
-              }}
-            />
-          </View>
-        </GestureDetector>
-      </GestureHandlerRootView>,
-    );
-  }
-  console.log("RENDER BUTTONS");
-  return <View style={{ flexDirection: "row", flexWrap: "wrap", gap: 5, padding: 15 }}>{rectangles}</View>;
 }
 
 const styles = StyleSheet.create({
