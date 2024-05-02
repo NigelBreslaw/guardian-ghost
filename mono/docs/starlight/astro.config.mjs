afee27a11ea47f3cc163f8501f59455277958f63
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-02 09:20:07 +0300

    docs: Show the transfer system flowchart

--- mono/docs/starlight/astro.config.mjs
+++ mono/docs/starlight/astro.config.mjs
@@ -1,35 +1,36 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.guardianghost.com',
-	integrations: [
-		starlight({
-			title: 'GG Docs',
-			social: {
-				email: 'mailto:support@guardianghost.com',
-				twitter: 'https://twitter.com/GGuardianGhost',
-				github: 'https://github.com/NigelBreslaw/guardian-ghost',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Monorepo', link: '/guides/monorepo/' },
-						{ label: 'Coding Guidelines', link: '/guides/coding-guidelines/' },
-						{ label: 'Infrastructure', link: '/guides/infrastructure/' },
-						{ label: 'Continuous Integration', link: '/guides/ci/' },
-						{ label: 'Automated Deployment', link: '/guides/automated-deployment/' },
-						{ label: 'Project Maintenance', link: '/guides/project-maintenance/' },
-						{ label: 'React Native', link: '/guides/react-native/' },
-						{ label: 'Costs', link: '/guides/costs/' },
-						{ label: 'Misc', link: '/guides/misc/' },
-						{ label: 'Authentication', link: '/guides/authentication/' },
-						{ label: 'To-do\'s', link: '/guides/todo/' }
-					],
-				},
-			],
-		}),
-	],
+  site: "https://docs.guardianghost.com",
+  integrations: [
+    starlight({
+      title: "GG Docs",
+      social: {
+        email: "mailto:support@guardianghost.com",
+        twitter: "https://twitter.com/GGuardianGhost",
+        github: "https://github.com/NigelBreslaw/guardian-ghost",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            { label: "Monorepo", link: "/guides/monorepo/" },
+            { label: "Coding Guidelines", link: "/guides/coding-guidelines/" },
+            { label: "Architecture", link: "/guides/architecture/" },
+            { label: "Infrastructure", link: "/guides/infrastructure/" },
+            { label: "Continuous Integration", link: "/guides/ci/" },
+            { label: "Automated Deployment", link: "/guides/automated-deployment/" },
+            { label: "Project Maintenance", link: "/guides/project-maintenance/" },
+            { label: "React Native", link: "/guides/react-native/" },
+            { label: "Costs", link: "/guides/costs/" },
+            { label: "Misc", link: "/guides/misc/" },
+            { label: "Authentication", link: "/guides/authentication/" },
+            { label: "To-do's", link: "/guides/todo/" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
