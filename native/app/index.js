49b3df9182390f11bf132d2a9c05276552423a11
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-02 23:01:22 +0300

    fix: Sentry is not enabled in dev

--- native/app/index.js
+++ native/app/index.js
@@ -10,8 +10,8 @@ Sentry.init({
   dsn: "https://7db2c06ee6ea56cae40a5bd963bad76b@o4506899216728065.ingest.us.sentry.io/4506899221970944",
 
   // Capture 100% of transactions in development, and 10% in production
-  tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 1.0,
-  enabled: process.env.NODE_ENV !== "development",
+  tracesSampleRate: 1.0,
+  enabled: !__DEV__,
   integrations: [new Sentry.ReactNativeTracing({ routingInstrumentation })],
   _experiments: {
     // profilesSampleRate is relative to tracesSampleRate.
