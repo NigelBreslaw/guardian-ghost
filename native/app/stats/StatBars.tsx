5eefa57f2dfb90ab2d65df3f655024eec54c6680
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-09 17:17:53 +0300

    feat: Numberic stats UI (#1575)

--- native/app/stats/StatBars.tsx
+++ native/app/stats/StatBars.tsx
@@ -30,9 +30,10 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 13,
     includeFontPadding: false,
-    textAlign: "right",
-    paddingRight: 10,
-    height: HEIGHT,
+    top: -1,
+    right: 10,
+    pointerEvents: "none",
+    position: "absolute",
   },
   valueText: {
     color: "white",
@@ -42,6 +43,43 @@ const styles = StyleSheet.create({
   },
 });
 
+type NumericUiProps = {
+  value: number;
+};
+
+function NumericUi(props: NumericUiProps) {
+  return (
+    <View style={{ height: HEIGHT, gap: 1 }}>
+      <Text style={styles.valueText}>{props.value}</Text>
+    </View>
+  );
+}
+
+type BarUiProps = {
+  value: number;
+};
+
+function BarUi(props: BarUiProps) {
+  const value = Math.min(props.value, 100);
+
+  return (
+    <View style={{ height: HEIGHT, flexDirection: "row", gap: 1 }}>
+      <View style={{ height: 10, width: 130, alignSelf: "center" }}>
+        <View style={{ flex: 1, backgroundColor: "white", opacity: 0.2 }} />
+        <View
+          style={{
+            position: "absolute",
+            width: (value / 100) * 130,
+            height: "100%",
+            backgroundColor: "white",
+          }}
+        />
+      </View>
+      <Text style={styles.valueText}>{value}</Text>
+    </View>
+  );
+}
+
 type StatBarsProps = {
   stats: ItemStats;
 };
@@ -49,33 +87,36 @@ type StatBarsProps = {
 export default function StatBars({ stats }: StatBarsProps) {
   const statUiData = DefaultWeaponStats;
   const labels = statUiData.map((UiData) => (
-    <View key={UiData.statType} style={{ height: HEIGHT }}>
+    <View
+      key={UiData.statType}
+      style={{
+        height: HEIGHT,
+        borderColor: "black",
+        borderWidth: 0.3,
+        alignContent: "flex-end",
+      }}
+    >
       <Text style={styles.text}>{StatType[UiData.statType]}</Text>
     </View>
   ));
 
   const bars = statUiData.map((UiData) => {
     const value = stats.get(UiData.statType) ?? 0;
-    return (
-      <View key={UiData.statType} style={{ height: HEIGHT, flexDirection: "row", gap: 1 }}>
-        <View style={{ height: 10, width: 130, alignSelf: "center" }}>
-          <View style={{ flex: 1, backgroundColor: "white", opacity: 0.2 }} />
-          <View
-            style={{
-              position: "absolute",
-              width: (value / 100) * 130,
-              height: "100%",
-              backgroundColor: "white",
-            }}
-          />
-        </View>
-        <Text style={styles.valueText}>{value}</Text>
-      </View>
-    );
+
+    switch (UiData.type) {
+      case "BAR":
+        return <BarUi key={UiData.statType} value={value} />;
+      case "NUMERAL":
+        return <NumericUi key={UiData.statType} value={value} />;
+      // case "RECOIL":
+      //   return <RecoilUi key={UiData.statType} value={value} />;
+      default:
+        return <BarUi key={UiData.statType} value={value} />;
+    }
   });
 
   return (
-    <View style={{ flexDirection: "row" }}>
+    <View style={{ flexDirection: "row", padding: 5 }}>
       <View style={{ flex: 1.5 }}>
         <View>{labels}</View>
       </View>
