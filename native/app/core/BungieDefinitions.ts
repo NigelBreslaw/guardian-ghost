b76e4f9e5077c461f1ddb85d49414b6eabe6e279
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-09 11:10:42 +0300

    feat: Control how stats are shown

--- native/app/core/BungieDefinitions.ts
+++ native/app/core/BungieDefinitions.ts
@@ -7,7 +7,7 @@ export type DefinitionKey = "DestinySocketCategoryDefinition" | "DestinyStatGrou
 // The self hosted minified JSON response definition
 // -------------------------------
 
-const MINI_ITEM_DEFINITION_VERSION = 7;
+const MINI_ITEM_DEFINITION_VERSION = 8;
 
 export const ItemResponseSchema = object({
   helpers: object(
--- native/app/stats/StatBars.tsx
+++ native/app/stats/StatBars.tsx
@@ -1,8 +1,30 @@
 import { StatType } from "@/app/bungie/Enums.ts";
 import type { ItemStats } from "@/app/stats/Logic.ts";
 import { View, Text, StyleSheet } from "react-native";
-const HEIGHT = 14;
 
+type UiStatType = "BAR" | "NUMERAL" | "RECOIL";
+
+type UiStatData = {
+  statType: StatType;
+  type: UiStatType;
+};
+
+// impact, range, stability, handling, reload speed, aim assistance, zoom, airborne, rounds per minute, magazine, recoil
+const DefaultWeaponStats: UiStatData[] = [
+  { statType: StatType.Impact, type: "BAR" },
+  { statType: StatType.Range, type: "BAR" },
+  { statType: StatType.Stability, type: "BAR" },
+  { statType: StatType.Handling, type: "BAR" },
+  { statType: StatType.ReloadSpeed, type: "BAR" },
+  { statType: StatType.AimAssistance, type: "BAR" },
+  { statType: StatType.Zoom, type: "BAR" },
+  { statType: StatType.AirborneEffectiveness, type: "BAR" },
+  { statType: StatType.RoundsPerMinute, type: "NUMERAL" },
+  { statType: StatType.Magazine, type: "NUMERAL" },
+  { statType: StatType.RecoilDirection, type: "RECOIL" },
+];
+
+const HEIGHT = 14;
 const styles = StyleSheet.create({
   text: {
     color: "white",
@@ -25,21 +47,32 @@ type StatBarsProps = {
 };
 
 export default function StatBars({ stats }: StatBarsProps) {
-  const labels = Array.from(stats, ([key]) => (
-    <View key={key} style={{ height: HEIGHT }}>
-      <Text style={styles.text}>{StatType[key]}</Text>
+  const statUiData = DefaultWeaponStats;
+  const labels = statUiData.map((UiData) => (
+    <View key={UiData.statType} style={{ height: HEIGHT }}>
+      <Text style={styles.text}>{StatType[UiData.statType]}</Text>
     </View>
   ));
 
-  const bars = Array.from(stats, ([key, value]) => (
-    <View key={key} style={{ height: HEIGHT, flexDirection: "row", gap: 1 }}>
-      <View style={{ height: 10, width: 130, alignSelf: "center" }}>
-        <View style={{ flex: 1, backgroundColor: "white", opacity: 0.2 }} />
-        <View style={{ position: "absolute", width: (value / 100) * 130, height: "100%", backgroundColor: "white" }} />
+  const bars = statUiData.map((UiData) => {
+    const value = stats.get(UiData.statType) ?? 0;
+    return (
+      <View key={UiData.statType} style={{ height: HEIGHT, flexDirection: "row", gap: 1 }}>
+        <View style={{ height: 10, width: 130, alignSelf: "center" }}>
+          <View style={{ flex: 1, backgroundColor: "white", opacity: 0.2 }} />
+          <View
+            style={{
+              position: "absolute",
+              width: (value / 100) * 130,
+              height: "100%",
+              backgroundColor: "white",
+            }}
+          />
+        </View>
+        <Text style={styles.valueText}>{value}</Text>
       </View>
-      <Text style={styles.valueText}>{value}</Text>
-    </View>
-  ));
+    );
+  });
 
   return (
     <View style={{ flexDirection: "row" }}>
