37b584a367500a86b0b9268eda38f8758a8fd0d0
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-10 16:10:41 +0300

    feat: Show enhanced-trait perks icon

--- native/app/stats/PerkCircle.tsx
+++ native/app/stats/PerkCircle.tsx
@@ -1,9 +1,11 @@
 import { View } from "react-native";
 import { Image } from "expo-image";
+import { ENHANCED_TRAIT } from "@/app/inventory/logic/Constants.ts";
 
 type PerkCircleProps = {
   icon: string | undefined;
   isEnabled: boolean;
+  isEnhanced: boolean;
 };
 
 export default function PerkCircle(props: PerkCircleProps) {
@@ -21,6 +23,9 @@ export default function PerkCircle(props: PerkCircleProps) {
       }}
     >
       <Image source={props.icon} style={{ width: 30, height: 30 }} />
+      {props.isEnhanced && (
+        <Image source={ENHANCED_TRAIT} tintColor="#F4D158" style={{ width: 51, height: 51, position: "absolute" }} />
+      )}
     </View>
   );
 }
