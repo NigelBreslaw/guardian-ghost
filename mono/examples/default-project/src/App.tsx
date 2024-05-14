83c5a27fd720582c0e9c0bf71430347450e0a081
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-14 09:26:58 +0300

    Fix

--- mono/examples/default-project/src/App.tsx
+++ mono/examples/default-project/src/App.tsx
@@ -1,7 +1,7 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
@@ -11,12 +11,7 @@ const App: Component = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a class={styles.link} href="https://github.com/solidjs/solid" target="_blank" rel="noopener noreferrer">
           Learn Solid
         </a>
       </header>
