88223a2163f43744a614f55378327c5356fe4cbc
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-21 12:36:54 +0300

    feat: tabs should be text on,y

--- native/app/inventory/pages/InventoryPages.tsx
+++ native/app/inventory/pages/InventoryPages.tsx
@@ -3,12 +3,7 @@ import ArmorPage from "@/app/inventory/pages/ArmorPage.tsx";
 import GeneralPage from "@/app/inventory/pages/GeneralPage.tsx";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Image } from "expo-image";
 import { StyleSheet, View } from "react-native";
-
-const WEAPONS_TAB_ICON = require("../../../images/weapons_tab.webp");
-const ARMOR_TAB_ICON = require("../../../images/armor_tab.webp");
-const GENERAL_TAB_ICON = require("../../../images/general_tab.webp");
 
 const Tab = createBottomTabNavigator();
 
@@ -17,40 +12,20 @@ function InventoryPages() {
     <View style={{ flex: 1 }}>
       <Tab.Navigator
         detachInactiveScreens={true}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused }) => {
-            switch (route.name) {
-              case "tab-weapons":
-                return (
-                  <Image
-                    source={WEAPONS_TAB_ICON}
-                    cachePolicy="memory"
-                    style={{ width: 20, height: 20, opacity: focused ? 1 : 0.4 }}
-                  />
-                );
-              case "tab-armor":
-                return (
-                  <Image
-                    source={ARMOR_TAB_ICON}
-                    cachePolicy="memory"
-                    style={{ width: 20, height: 20, opacity: focused ? 1 : 0.4 }}
-                  />
-                );
-              case "tab-inventory":
-                return (
-                  <Image
-                    source={GENERAL_TAB_ICON}
-                    cachePolicy="memory"
-                    style={{ width: 20, height: 20, opacity: focused ? 1 : 0.4 }}
-                  />
-                );
-            }
-          },
+        screenOptions={() => ({
           tabBarActiveTintColor: "white",
           tabBarInactiveTintColor: "gray",
           tabBarStyle: {
             borderTopColor: "grey",
             borderTopWidth: StyleSheet.hairlineWidth,
+            height: 60,
+          },
+          tabBarIconStyle: {
+            display: "none",
+          },
+          tabBarLabelStyle: {
+            alignSelf: "center",
+            fontSize: 13,
           },
         })}
       >
