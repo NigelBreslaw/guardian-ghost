24dd9eaf149d5cd511a6496371fa8e8b1f7e92a6
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-23 21:10:30 +0300

    fix: Correctly enable the React Compiler

--- native/babel.config.js
+++ native/babel.config.js
@@ -2,16 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    env: {
-      production: {
-        plugins: [
-          "babel-plugin-react-compiler",
-          {
-            runtimeModule: "react-compiler-runtime",
-          },
-          "react-native-reanimated/plugin",
-        ],
-      },
-    },
+    plugins: [
+      [
+        "babel-plugin-react-compiler",
+        {
+          compilationMode: "annotation",
+          runtimeModule: "react-compiler-runtime",
+        },
+
+        "react-native-reanimated/plugin",
+      ],
+    ],
   };
 };
