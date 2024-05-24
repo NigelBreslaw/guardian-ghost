722c764618c1934b5a2178b7119f1c57bdf164ad
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-05-24 17:58:12 +0300

    refactor: Apply same fix to mods + consumables (#1742)

--- native/app/store/UIDataSlice.ts
+++ native/app/store/UIDataSlice.ts
@@ -53,13 +53,13 @@ function getVaultSpacerSize(get: UIDataSliceGetter, bucket: SectionBuckets): num
   if (bucket === SectionBuckets.Consumables) {
     const totalConsumables = consumables.length;
     const totalRows = Math.ceil(totalConsumables / 5);
-    return ICON_SIZE * totalRows + ICON_MARGIN * (totalRows - 1);
+    return (ICON_SIZE + ICON_MARGIN) * totalRows;
   }
 
   if (bucket === SectionBuckets.Mods) {
     const totalMods = mods.length;
     const totalRows = Math.ceil(totalMods / 5);
-    return ICON_SIZE * totalRows + ICON_MARGIN * (totalRows - 1);
+    return (ICON_SIZE + ICON_MARGIN) * totalRows;
   }
   return 0;
 }
