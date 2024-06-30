405bb001e5bc8314ce4c5810d2b28d5e24950769
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-06-30 15:26:23 +0300

    feat: Show class and race name

--- native/app/Root.tsx
+++ native/app/Root.tsx
@@ -12,7 +12,7 @@ import { useGGStore } from "@/app/store/GGStore.ts";
 import { bungieManifestSchema } from "@/app/core/ApiResponse.ts";
 import type { DestinyItemIdentifier } from "@/app/inventory/logic/Helpers.ts";
 import App from "@/app/App"; // Do not use the file extension or the web version will fail to be used.
-import { updateBucketSizes } from "@/app/utilities/Constants.ts";
+import { updateBucketSizes, updateDestinyText } from "@/app/utilities/Constants.ts";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -34,6 +34,7 @@ async function init() {
     await useGGStore.getState().loadCustomDefinitions(parsedManifest.version);
     await useGGStore.getState().loadBungieDefinitions(parsedBungieManifest);
     updateBucketSizes();
+    updateDestinyText();
   } catch (e) {
     // If the network call fails try to use the already downloaded version.
     console.error("Failed to load custom manifest", e);
