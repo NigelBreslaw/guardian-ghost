61b543476571dac7d00186e8ef95468a6737d4ca
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-07-04 00:12:40 +0300

    chore: Add svg support (#1934)
    * chore: Add svg support
    
    * Fix for ts-reset

--- native/metro.config.js
+++ native/metro.config.js
@@ -2,4 +2,8 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 const config = getSentryExpoConfig(__dirname);
 
+config.resolver.sourceExts.push("svg");
+
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+
 module.exports = config;
