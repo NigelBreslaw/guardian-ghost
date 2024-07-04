5f6f34c03e7b06c51a84e382d5309f82a06fca16
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-07-04 16:01:47 +0300

    fix: Logout still left old data behind (#1944)
    Plus a react issue where the header doesn't update when there are no longer characters. So now on logout you cannot see the old header.

--- native/app/inventory/pages/InventoryHeader.tsx
+++ native/app/inventory/pages/InventoryHeader.tsx
@@ -4,8 +4,14 @@ import { Image } from "expo-image";
 import { useGGStore } from "@/app/store/GGStore.ts";
 
 export default function InventoryHeader() {
+  "use memo";
+  const totalCharacters = useGGStore((state) => state.ggCharacters.length);
   const currentListIndex = useGGStore((state) => state.currentListIndex);
   const characterBackgroundEmblem = useGGStore((state) => state.ggCharacters[currentListIndex]?.secondarySpecial);
+
+  if (totalCharacters === 0) {
+    return null;
+  }
 
   return (
     <View style={{ flex: 1 }}>
