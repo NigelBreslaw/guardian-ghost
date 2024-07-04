13b7b4bd3740479a2df6b21c3d8e653500be98d4
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-07-04 12:46:43 +0200

    And web

--- native/metro.config.js
+++ native/metro.config.js
@@ -4,7 +4,6 @@ const config = getSentryExpoConfig(__dirname);
 
 config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== "svg");
 config.resolver.sourceExts.push("svg");
-
 config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
 
 module.exports = config;
