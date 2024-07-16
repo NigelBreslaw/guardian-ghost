20513df29bebd50f2e90cabfd38f16cec1173a69
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-07-16 17:13:14 +0300

    ux: Add menu icons (#2034)

---
+++ native/lib/icons/Refresh-ccw.tsx
@@ -0,0 +1,4 @@
+import { RefreshCcw } from "lucide-react-native";
+import { iconWithClassName } from "./iconWithClassName";
+iconWithClassName(RefreshCcw);
+export { RefreshCcw };
---
+++ native/lib/icons/Shield.tsx
@@ -0,0 +1,4 @@
+import { Shield } from "lucide-react-native";
+import { iconWithClassName } from "./iconWithClassName";
+iconWithClassName(Shield);
+export { Shield };
