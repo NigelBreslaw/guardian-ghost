d8640a2634f858a61c2f9418e20d8dc8cb2933f3
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-07-17 20:07:19 +0200

    feat: Support persisting zustand state

--- native/app/store/GGStore.ts
+++ native/app/store/GGStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
-import { subscribeWithSelector } from "zustand/middleware";
+import { subscribeWithSelector, persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { type AccountSlice, createAccountSlice } from "./Account/AccountSlice.ts";
 import { type AuthenticationSlice, createAuthenticationSlice } from "./Authentication/AuthenticationSlice.ts";
@@ -10,11 +11,18 @@ import { type SettingsSlice, createSettingsSlice } from "./SettingsSlice.ts";
 export interface IStore extends AccountSlice, AuthenticationSlice, DefinitionsSlice, UIDataSlice, SettingsSlice {}
 
 export const useGGStore = create<IStore>()(
-  subscribeWithSelector((...a) => ({
-    ...createAccountSlice(...a),
-    ...createAuthenticationSlice(...a),
-    ...createDefinitionsSlice(...a),
-    ...createUIDataSlice(...a),
-    ...createSettingsSlice(...a),
-  })),
+  persist(
+    subscribeWithSelector((...a) => ({
+      ...createAccountSlice(...a),
+      ...createAuthenticationSlice(...a),
+      ...createDefinitionsSlice(...a),
+      ...createUIDataSlice(...a),
+      ...createSettingsSlice(...a),
+    })),
+    {
+      name: "gg-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: (state) => ({ currentListIndex: state.currentListIndex }),
+    },
+  ),
 );
