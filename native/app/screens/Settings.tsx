9fb8080b5f3b044f96c2415077929efcd2f7f666
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-07-19 18:24:41 +0200

    feat: Bottom sheet settings work (#2079)

--- native/app/screens/Settings.tsx
+++ native/app/screens/Settings.tsx
@@ -1,12 +1,13 @@
-import * as React from "react";
 import { View } from "react-native";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { cn } from "@/lib/utils.ts";
+import { useGGStore } from "@/app/store/GGStore.ts";
+import type { ShowBottomSheetStringValues } from "@/app/store/SettingsSlice.ts";
 
 export default function Settings() {
-  const [value, setValue] = React.useState("Auto");
-
+  const showBottomSheetPreference = useGGStore((state) => state.showBottomSheetPreference);
+  const setBottomSheetPreference = useGGStore((state) => state.setBottomSheetPreference);
   return (
     <View className="flex-1 p-6  gap-3">
       <View>
@@ -14,26 +15,36 @@ export default function Settings() {
           Show the transfer buttons
         </Label>
       </View>
-      <RadioGroup value={value} onValueChange={setValue} className="gap-3">
-        <RadioGroupItemWithLabel value="Auto" onLabelPress={() => setValue("Auto")} />
-        <RadioGroupItemWithLabel value="Always showing" onLabelPress={() => setValue("Always showing")} />
-        <RadioGroupItemWithLabel value="Always minimized" onLabelPress={() => setValue("Always minimized")} />
+      <RadioGroup value={showBottomSheetPreference} onValueChange={setBottomSheetPreference} className="gap-3">
+        <RadioGroupItemWithLabel value={"AUTOMATIC"} onLabelPress={setBottomSheetPreference} label="Auto" />
+        <RadioGroupItemWithLabel
+          value={"ALWAYS_SHOWING"}
+          onLabelPress={setBottomSheetPreference}
+          label="Always showing"
+        />
+        <RadioGroupItemWithLabel
+          value={"ALWAYS_MINIMIZED"}
+          onLabelPress={setBottomSheetPreference}
+          label="Always minimized"
+        />
       </RadioGroup>
     </View>
   );
 }
 function RadioGroupItemWithLabel({
   value,
+  label,
   onLabelPress,
 }: {
-  value: string;
-  onLabelPress: () => void;
+  value: ShowBottomSheetStringValues;
+  label: string;
+  onLabelPress: (ShowBottomSheetPreference: string) => void;
 }) {
   return (
     <View className={"flex-row gap-2 items-center"}>
       <RadioGroupItem aria-labelledby={`label-for-${value}`} value={value} />
-      <Label nativeID={`label-for-${value}`} onPress={onLabelPress}>
-        {value}
+      <Label nativeID={`label-for-${value}`} onPress={() => onLabelPress(value)}>
+        {label}
       </Label>
     </View>
   );
--- native/app/store/SettingsSlice.ts
+++ native/app/store/SettingsSlice.ts
@@ -4,11 +4,13 @@ import type { StateCreator } from "zustand";
 export type SettingsSliceSetter = Parameters<StateCreator<IStore, [], [], SettingsSlice>>[0];
 export type SettingsSliceGetter = Parameters<StateCreator<IStore, [], [], SettingsSlice>>[1];
 
-enum ShowBottomSheetPreference {
+export enum ShowBottomSheetPreference {
   Auto = "AUTOMATIC",
   AlwaysShowing = "ALWAYS_SHOWING",
   AlwaysMinimized = "ALWAYS_MINIMIZED",
 }
+
+export type ShowBottomSheetStringValues = "AUTOMATIC" | "ALWAYS_SHOWING" | "ALWAYS_MINIMIZED";
 
 export enum ShowBottomSheet {
   show = "show",
@@ -18,12 +20,28 @@ export enum ShowBottomSheet {
 export interface SettingsSlice {
   showBottomSheetPreference: ShowBottomSheetPreference;
   showNextBottomSheet: ShowBottomSheet;
+  setBottomSheetPreference: (showBottomSheetPreference: string) => void;
   setShowBottomSheet: (showBottomSheet: ShowBottomSheet) => void;
 }
 
 export const createSettingsSlice: StateCreator<IStore, [], [], SettingsSlice> = (set, get) => ({
   showBottomSheetPreference: ShowBottomSheetPreference.Auto,
   showNextBottomSheet: ShowBottomSheet.show,
+  setBottomSheetPreference: (showBottomSheetPreference: string) => {
+    if (get().showBottomSheetPreference !== showBottomSheetPreference) {
+      switch (showBottomSheetPreference) {
+        case ShowBottomSheetPreference.Auto:
+          set({ showBottomSheetPreference });
+          break;
+        case ShowBottomSheetPreference.AlwaysShowing:
+          set({ showBottomSheetPreference, showNextBottomSheet: ShowBottomSheet.show });
+          break;
+        case ShowBottomSheetPreference.AlwaysMinimized:
+          set({ showBottomSheetPreference, showNextBottomSheet: ShowBottomSheet.minimize });
+          break;
+      }
+    }
+  },
   setShowBottomSheet: (showBottomSheet: ShowBottomSheet) => {
     if (get().showBottomSheetPreference === ShowBottomSheetPreference.Auto)
       set({ showNextBottomSheet: showBottomSheet });
