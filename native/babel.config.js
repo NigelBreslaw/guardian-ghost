5e98485488fbf85676b2ad30ab4d3d6b1f985ac2
Nigel Breslaw <nigel.breslaw@gmail.com>
2024-07-22 15:31:31 +0300

    fix: Options menu was buggy
    On Web it would appear twice. Under certain conditions it would break in dev for mobile.
    
    This fix stops the Options menu being created 3 times. It also changes the setup to fully match the setup in the docs.

--- native/babel.config.js
+++ native/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "nativewind/babel"],
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
     plugins: [
       [
         "babel-plugin-react-compiler",
