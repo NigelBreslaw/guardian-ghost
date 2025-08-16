d8e8b315190a277aef201824afe13d7389e16de5
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-08-16 16:30:02 +0300

    Stop update defs also building the web site

--- native/metro.config.js
+++ native/metro.config.js
@@ -6,4 +6,16 @@ config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== "s
 config.resolver.sourceExts.push("svg");
 config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
 
+// Add wasm asset support
+config.resolver.assetExts.push("wasm");
+
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    middleware(req, res, next);
+  };
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
