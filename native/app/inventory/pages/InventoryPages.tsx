24863c048d5f1930183f7f7cbbf47faacab52e3c
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-10-11 22:47:14 +0300

    Use native tabs

--- native/app/inventory/pages/InventoryPages.tsx
+++ native/app/inventory/pages/InventoryPages.tsx
@@ -1,6 +1,5 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Platform, StyleSheet, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { createNativeBottomTabNavigator } from "@bottom-tabs/react-navigation";
+import { Platform, View } from "react-native";
 import * as Haptics from "expo-haptics";
 
 import WeaponsPage from "@/app/inventory/pages/WeaponsPage.tsx";
@@ -8,7 +7,6 @@ import ArmorPage from "@/app/inventory/pages/ArmorPage.tsx";
 import GeneralPage from "@/app/inventory/pages/GeneralPage.tsx";
 import { InventoryPageEnums } from "@/app/inventory/logic/Helpers.ts";
 import { useGGStore } from "@/app/store/GGStore.ts";
-import { Pressable, type PressableProps } from "react-native-gesture-handler";
 import OptionsMenu from "@/components/ui/OptionsMenu.tsx";
 
 function pageEnumToPageName(pageEnum: InventoryPageEnums): string {
@@ -23,102 +21,49 @@ function pageEnumToPageName(pageEnum: InventoryPageEnums): string {
   return "tab-weapons";
 }
 
-const Tab = createBottomTabNavigator();
+const Tab = createNativeBottomTabNavigator();
 
 export default function InventoryPages() {
   "use memo";
-  const insets = useSafeAreaInsets();
-  // The hack on height and marginBottom is because something is bugged with either this component
-  // layout or the bottom tab bar. The height depends on the existence of a safeArea. Web, android and
-  // older iOS devices with no bottom safeArea will size the bar differently to iOS devices with a safeArea.
-  // So the height now depends on this safeArea being larger than zero.
 
   return (
     <View style={{ flex: 1 }}>
       <OptionsMenu />
       <Tab.Navigator
-        detachInactiveScreens={true}
         initialRouteName={pageEnumToPageName(useGGStore.getState().currentInventoryPage)}
-        screenOptions={() => ({
-          tabBarAllowFontScaling: false,
-          lazy: false,
-          tabBarActiveTintColor: "white",
-          tabBarInactiveTintColor: "gray",
-          tabBarStyle: {
-            borderTopColor: "grey",
-            borderTopWidth: StyleSheet.hairlineWidth,
-            height: insets.bottom > 0 ? 70 : 50,
+        tabBarActiveTintColor="white"
+        tabBarInactiveTintColor="gray"
+        tabBarStyle={{
+          backgroundColor: "#17101F",
+        }}
+        screenListeners={{
+          tabPress: () => {
+            if (Platform.OS !== "web") {
+              Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+            }
           },
-          tabBarIconStyle: {
-            display: "none",
-          },
-          tabBarLabelStyle: {
-            fontSize: 13,
-          },
-        })}
+        }}
       >
         <Tab.Screen
           name="tab-weapons"
-          options={{
-            tabBarLabel: "Weapons",
-            tabBarButton: (props) => (
-              <Pressable
-                {...(props as PressableProps)}
-                style={[{ alignSelf: "center", justifyContent: "center", height: "100%" }]}
-                onPressIn={() => {
-                  if (Platform.OS !== "web") {
-                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-                  }
-                }}
-              />
-            ),
-            headerStyle: {
-              height: 0,
-            },
-          }}
           component={WeaponsPage}
+          options={{
+            title: "Weapons",
+          }}
         />
         <Tab.Screen
           name="tab-armor"
-          options={{
-            tabBarLabel: "Armor",
-            tabBarButton: (props) => (
-              <Pressable
-                {...(props as PressableProps)}
-                style={[{ alignSelf: "center", justifyContent: "center", height: "100%" }]}
-                onPressIn={() => {
-                  if (Platform.OS !== "web") {
-                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-                  }
-                }}
-              />
-            ),
-            headerStyle: {
-              height: 0,
-            },
-          }}
           component={ArmorPage}
+          options={{
+            title: "Armor",
+          }}
         />
         <Tab.Screen
           name="tab-inventory"
-          options={{
-            tabBarLabel: "Inventory",
-            tabBarButton: (props) => (
-              <Pressable
-                {...(props as PressableProps)}
-                style={[{ alignSelf: "center", justifyContent: "center", height: "100%" }]}
-                onPressIn={() => {
-                  if (Platform.OS !== "web") {
-                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-                  }
-                }}
-              />
-            ),
-            headerStyle: {
-              height: 0,
-            },
-          }}
           component={GeneralPage}
+          options={{
+            title: "Inventory",
+          }}
         />
       </Tab.Navigator>
     </View>
