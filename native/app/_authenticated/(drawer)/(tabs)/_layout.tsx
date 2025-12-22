a62615fcf099a3058b7c3c2241b3796f55253fdc
Nigel Breslaw <nigel.breslaw@gmail.com>
2025-12-22 18:02:40 +0200

    Update to native tabs (#3495)
    * Update SDK
    
    * Native tabs
    
    * Fix
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- native/app/_authenticated/(drawer)/(tabs)/_layout.tsx
+++ native/app/_authenticated/(drawer)/(tabs)/_layout.tsx
@@ -1,87 +1,44 @@
-import { Tabs } from "expo-router";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 import { Platform, View } from "react-native";
-import { Image } from "expo-image";
-import { InventoryPageEnums } from "@/app/inventory/logic/Helpers.ts";
-import { useGGStore } from "@/app/store/GGStore.ts";
 import OptionsMenu from "@/components/ui/OptionsMenu.tsx";
-
-function pageEnumToPageName(pageEnum: InventoryPageEnums): string {
-  switch (pageEnum) {
-    case InventoryPageEnums.Armor:
-      return "armor";
-    case InventoryPageEnums.General:
-      return "inventory";
-    case InventoryPageEnums.Weapons:
-      return "weapons";
-  }
-  return "weapons";
-}
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function TabsLayout() {
   "use memo";
 
-  const initialRouteName = pageEnumToPageName(useGGStore.getState().currentInventoryPage);
-
   return (
     <View style={{ flex: 1 }}>
       {Platform.OS !== "ios" && <OptionsMenu />}
-      <Tabs
-        initialRouteName={initialRouteName}
-        screenOptions={{
-          tabBarActiveTintColor: "orange",
-          tabBarInactiveTintColor: "gray",
-          tabBarStyle: {
-            backgroundColor: "#17101F",
-          },
-          headerShown: false, // Headers are now handled by drawer
+      <NativeTabs
+        backgroundColor="#17101F"
+        iconColor={{
+          default: "gray",
+          selected: "orange",
+        }}
+        labelStyle={{
+          default: { color: "gray" },
+          selected: { color: "orange" },
         }}
       >
-        <Tabs.Screen
-          name="weapons"
-          options={{
-            title: "Weapons",
-            tabBarIcon: ({ color, size }) => {
-              // eslint-disable-next-line @typescript-eslint/no-require-imports
-              return (
-                <Image
-                  source={require("../../../../images/sword.svg")}
-                  style={{ width: size || 24, height: size || 24, tintColor: color }}
-                />
-              );
-            },
-          }}
-        />
-        <Tabs.Screen
-          name="armor"
-          options={{
-            title: "Armor",
-            tabBarIcon: ({ color, size }) => {
-              // eslint-disable-next-line @typescript-eslint/no-require-imports
-              return (
-                <Image
-                  source={require("../../../../images/shield.svg")}
-                  style={{ width: size || 24, height: size || 24, tintColor: color }}
-                />
-              );
-            },
-          }}
-        />
-        <Tabs.Screen
-          name="inventory"
-          options={{
-            title: "Inventory",
-            tabBarIcon: ({ color, size }) => {
-              // eslint-disable-next-line @typescript-eslint/no-require-imports
-              return (
-                <Image
-                  source={require("../../../../images/package.svg")}
-                  style={{ width: size || 24, height: size || 24, tintColor: color }}
-                />
-              );
-            },
-          }}
-        />
-      </Tabs>
+        <NativeTabs.Trigger name="weapons">
+          <NativeTabs.Trigger.Label>Weapons</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="sword" />}
+          />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="armor">
+          <NativeTabs.Trigger.Label>Armor</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="shield-half-full" />}
+          />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="inventory">
+          <NativeTabs.Trigger.Label>Inventory</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="package-variant-closed" />}
+          />
+        </NativeTabs.Trigger>
+      </NativeTabs>
     </View>
   );
 }
